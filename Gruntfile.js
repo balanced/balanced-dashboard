ed8de1213560cff9bd20c31f46306284abbc2f34
Balanced Marshall <marshall@poundpay.com>
2013-03-18 16:48:15 -0700

    run less task at startup

--- Gruntfile.js
+++ Gruntfile.js
@@ -183,5 +183,5 @@ module.exports = function (grunt) {
      Default task. Compiles templates, neuters application code, and begins
      watching for changes.
      */
-    grunt.registerTask('default', ['ember_templates', 'neuter', 'watch']);
+    grunt.registerTask('default', ['ember_templates', 'neuter', 'less', 'watch']);
 };
