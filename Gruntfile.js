c39bed789ef7f0d709725fe88d191e09e25369a9
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-04-17 17:00:07 -0700

    Add test coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,6 +121,15 @@ module.exports = function (grunt) {
          Prints the report in your terminal.
          */
         qunit: {
+            options: {
+              '--web-security': 'no',
+              coverage: {
+                src: ['build/dashboard-prod.js'],
+                instrumentedFiles: 'temp/',
+                htmlReport: 'report/coverage',
+                coberturaReport: 'report/'
+              }
+            },
             all: ['test/**/*.html']
         },
 
@@ -286,7 +295,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-neuter');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-ember-templates');
@@ -296,6 +304,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-s3');
+    grunt.loadNpmTasks('grunt-qunit-istanbul');
 
     /*
      A task to build the test runner html file that get place in
