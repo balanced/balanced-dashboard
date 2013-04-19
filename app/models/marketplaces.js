d8d2c8a4644eb97e27c3de80590c90e6a12ab340
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-04-19 16:38:04 -0700

    Simple iframe for marketplace overview

--- app/models/marketplaces.js
+++ app/models/marketplaces.js
@@ -3,5 +3,13 @@ require('app/models/store');
 Balanced.Marketplace = Balanced.Model.extend({
     name: DS.attr('string'),
     in_escrow: DS.attr('number'),
-    uri: DS.attr('string')
+    uri: DS.attr('string'),
+
+    embedded_iframe_url: function() {
+        return ENV.BALANCED.WWW + "/marketplaces/" + this.get('id') + "?embedded=1";
+    }.property('id'),
+
+    invoices_embedded_iframe_url: function() {
+        return ENV.BALANCED.WWW + "/marketplaces/" + this.get('id') + "/invoices?embedded=1";
+    }.property('id')
 });
