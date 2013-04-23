1f8f00911a4199760123022ea12d01c0ca024e01
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-04-23 12:41:14 -0700

    Got credits, debits, refunds, and accounts working

--- app/routes/router.js
+++ app/routes/router.js
@@ -9,6 +9,10 @@ Balanced.Router.map(function () {
     this.route("logs", { path: "/logs" });
 
     this.route("credits", { path: "/credits/:credit_id"});
+    this.route("debits", { path: "/debits/:debit_id"});
+    this.route("refunds", { path: "/refunds/:refund_id"});
+
+    this.route("accounts", { path: "/accounts/:account_id"});
   });
   this.resource('login');
 });
@@ -30,11 +34,27 @@ Balanced.ApplicationRoute = Balanced.Route.extend({
 Balanced.MarketplaceCreditsRoute = Balanced.Route.extend({
   model: function(params) {
     var marketplace = this.modelFor('marketplace');
-    // TODO - should be finding the real one, not creating it.
-    var credit = Balanced.Credit.createRecord({
-      id: params.credit_id,
-      marketplace: marketplace
-    });
-    return credit;
+    return ENV.BALANCED.WWW + "/marketplaces/" + marketplace.get('id') + "/credits/" + params.credit_id + "?embedded=1";
+  }
+});
+
+Balanced.MarketplaceDebitsRoute = Balanced.Route.extend({
+  model: function(params) {
+    var marketplace = this.modelFor('marketplace');
+    return ENV.BALANCED.WWW + "/marketplaces/" + marketplace.get('id') + "/debits/" + params.debit_id + "?embedded=1";
+  }
+});
+
+Balanced.MarketplaceRefundsRoute = Balanced.Route.extend({
+  model: function(params) {
+    var marketplace = this.modelFor('marketplace');
+    return ENV.BALANCED.WWW + "/marketplaces/" + marketplace.get('id') + "/refunds/" + params.refund_id + "?embedded=1";
+  }
+});
+
+Balanced.MarketplaceAccountsRoute = Balanced.Route.extend({
+  model: function(params) {
+    var marketplace = this.modelFor('marketplace');
+    return ENV.BALANCED.WWW + "/marketplaces/" + marketplace.get('id') + "/accounts/" + params.account_id + "?embedded=1";
   }
 });
