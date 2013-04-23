d94188260a192f9a0314a59fb7454a537613a02e
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-04-23 12:53:02 -0700

    Fix link and test errors

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,11 @@
 Balanced.ApplicationController = Ember.Controller.extend({
   iframeLinkClicked: function(context) {
     var transitionToDest = context.url;
-    if(transitionToDest.indexOf('?') != -1) {
+    if(transitionToDest.indexOf('?') !== -1) {
       transitionToDest = transitionToDest.substring(0, transitionToDest.indexOf('?'));
     }
 
     if(transitionToDest !== '#') {
-      console.log("Detected link click - going to " + transitionToDest);
       window.location.hash = "#" + transitionToDest;
     }
   }
