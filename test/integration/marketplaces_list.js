98796b9009296603f6ee19794f131a40269bd0b5
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-04-23 12:53:02 -0700

    Fix link and test errors

--- test/integration/marketplaces_list.js
+++ test/integration/marketplaces_list.js
@@ -1,14 +1,11 @@
 module("Marketplaces List");
 
-test("view all marketplaces", function () {
-    $("a:contains('Marketplaces')").click();
-
-    equal($("#marketplace-list").find('a').first().text(), "Test Marketplace");
+test("view marketplace list", function () {
+    equal($("#marketplaces ul").find('a').first().text(), "Test Marketplace");
 });
 
 test("view single marketplace", function () {
-    $("a:contains('Marketplaces')").click();
-    $("a:contains('Test Marketplace')").click();
+    $("#marketplaces ul a:contains('Second Test Marketplace')").click();
 
-    equal($(".marketplace-name").text(), "Test Marketplace");
+    equal($(".marketplace-name").text(), "Second Test Marketplace");
 });
