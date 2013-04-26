bb8abcee92b819c70fcfbc7987ce2130826e63b3
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-04-26 10:23:31 -0700

    Moved onLoad out of HTML and into JS, consolidated JS into the view and out of the global namespace

--- app/views/embedded_iframe.js
+++ app/views/embedded_iframe.js
@@ -4,28 +4,28 @@ Balanced.EmbeddedIframeView = Balanced.View.extend({
   didInsertElement: function() {
     $('iframe').iframeAutoHeight({debug: true});
     $('iframe.auto-height').iframeAutoHeight({minHeight: 400});
+
+    var _this = this;
+    $("#embedded-dashboard-content").load(function() {
+      // Fire this in case the server redirected
+      _this.updateHashFromIframeLocation(this.contentWindow.location.pathname);
+
+      // Add a handler to links so we can change the page BEFORE the page loads
+      $("#embedded-dashboard-content").contents().find("a").click(function(event) {
+        var addressValue = $(this).attr("href");
+        _this.updateHashFromIframeLocation(addressValue);
+      });
+    })
+  },
+
+  updateHashFromIframeLocation: function(iframePath) {
+    var transitionToDest = iframePath;
+    if(transitionToDest.indexOf('?') !== -1) {
+      transitionToDest = transitionToDest.substring(0, transitionToDest.indexOf('?'));
+    }
+
+    if(transitionToDest !== '#') {
+      window.location.hash = "#" + transitionToDest;
+    }
   }
 });
-
-Balanced.update_hash_from_iframe_location = function(iframePath) {
-  var transitionToDest = iframePath;
-  if(transitionToDest.indexOf('?') !== -1) {
-    transitionToDest = transitionToDest.substring(0, transitionToDest.indexOf('?'));
-  }
-
-  if(transitionToDest !== '#') {
-    window.location.hash = "#" + transitionToDest;
-  }
-}
-
-// can't attach the onload using jquery, so have to statically define this here so it can be referenced in the HTML template
-Balanced.embedded_iframe_loaded = function(location) {
-  // Fire this in case the server redirected
-  Balanced.update_hash_from_iframe_location(location.pathname);
-
-  // Add a handler to links so we can change the page BEFORE the page loads
-  $("#embedded-dashboard-content").contents().find("a").click(function(event) {
-    var addressValue = $(this).attr("href");
-    Balanced.update_hash_from_iframe_location(addressValue);
-  });
-};
