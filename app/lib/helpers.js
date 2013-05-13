5ed3fdf9b8a9a1b08a1acbdb0ccab5bd7f962c05
Marshall Jones <marshall@balancedpayments.com>
2013-05-13 14:33:25 -0700

    checkpoint

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -132,6 +132,16 @@ $.expr[":"].icontains = $.expr.createPseudo(function (arg) {
     };
 });
 
+// http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format/4673436#4673436
+if (!String.prototype.format) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
+}
+
 Balanced.Helpers = (function () {
 
     // example of how to parse and format iso8601 dates. will parse the
@@ -162,7 +172,7 @@ Balanced.Helpers = (function () {
 })();
 
 Balanced.Utils = {
-    uriToDashboardFragment: function(uri) {
+    uriToDashboardFragment: function (uri) {
         // have to strip off the API version
         return uri.substring(3);
     }
