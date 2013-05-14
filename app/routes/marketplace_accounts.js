acb1a7a8c073a386d10c12491135e12a44adb561
Marshall Jones <marshall@balancedpayments.com>
2013-05-14 14:06:27 -0700

    jshint

--- app/routes/marketplace_accounts.js
+++ app/routes/marketplace_accounts.js
@@ -1,26 +1,25 @@
 var model = function () {
-   return ENV.BALANCED.WWW + document.location.hash.substr(1) + '?embedded=1';
-}
+    return ENV.BALANCED.WWW + document.location.hash.substr(1) + '?embedded=1';
+};
 
 Balanced.AccountsIndexRoute = Balanced.AuthRoute.extend({
-  model: model
+    model: model
 });
 
 Balanced.AccountsCreditRoute = Balanced.AuthRoute.extend({
-  model: model
+    model: model
 });
 
 Balanced.AccountsDebitRoute = Balanced.AuthRoute.extend({
-  model: model
+    model: model
 });
 
 Balanced.AccountsRefundRoute = Balanced.AuthRoute.extend({
-  model: model
+    model: model
 });
 
 Balanced.AccountsHoldRoute = Balanced.AuthRoute.extend({
-  model: model,
-
+    model: model
 });
 
 ////
@@ -28,29 +27,29 @@ Balanced.AccountsHoldRoute = Balanced.AuthRoute.extend({
 //
 ////
 Balanced.MarketplaceCreditsRoute = Balanced.AuthRoute.extend({
-  model: model,
-  renderTemplate: function() {
-    this.render('accounts/credit');
-  }
+    model: model,
+    renderTemplate: function () {
+        this.render('accounts/credit');
+    }
 });
 
 Balanced.MarketplaceDebitsRoute = Balanced.AuthRoute.extend({
-  model: model,
-  renderTemplate: function() {
-    this.render('accounts/debit');
-  }
+    model: model,
+    renderTemplate: function () {
+        this.render('accounts/debit');
+    }
 });
 
 Balanced.MarketplaceRefundsRoute = Balanced.AuthRoute.extend({
-  model: model,
-  renderTemplate: function() {
-    this.render('accounts/refund');
-  }
+    model: model,
+    renderTemplate: function () {
+        this.render('accounts/refund');
+    }
 });
 
 Balanced.MarketplaceHoldsRoute = Balanced.AuthRoute.extend({
-  model: model,
-  renderTemplate: function() {
-    this.render('accounts/hold');
-  }
+    model: model,
+    renderTemplate: function () {
+        this.render('accounts/hold');
+    }
 });
