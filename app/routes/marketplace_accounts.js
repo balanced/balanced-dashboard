e3558bce99afba0ba62d40836467c7b50a30d523
Justin <justin@nodesocket.com>
2013-05-14 13:57:38 -0700

    Hack fixed alias for credit, debit, hold, refund routes.

--- app/routes/marketplace_accounts.js
+++ app/routes/marketplace_accounts.js
@@ -1,63 +1,56 @@
+var model = function () {
+   return ENV.BALANCED.WWW + document.location.hash.substr(1) + '?embedded=1';
+}
+
 Balanced.AccountsIndexRoute = Balanced.AuthRoute.extend({
-  model: function(params) {
-    var marketplace = this.modelFor('marketplace');
-    return marketplace.get('web_uri') + "/accounts/" + params.account_id + "?embedded=1";
-  }
+  model: model
 });
 
 Balanced.AccountsCreditRoute = Balanced.AuthRoute.extend({
-  model: function(params) {
-    var marketplace = this.modelFor('marketplace');
-    return marketplace.get('web_uri') + "/accounts/" + params.account_id +  "/credits/" + params.credit_id + "?embedded=1";
-  }
+  model: model
 });
 
 Balanced.AccountsDebitRoute = Balanced.AuthRoute.extend({
-  model: function(params) {
-    var marketplace = this.modelFor('marketplace');
-    return marketplace.get('web_uri') + "/accounts/" + params.account_id +  "/debits/" + params.debit_id + "?embedded=1";
-  }
+  model: model
 });
 
 Balanced.AccountsRefundRoute = Balanced.AuthRoute.extend({
-  model: function(params) {
-    var marketplace = this.modelFor('marketplace');
-    return marketplace.get('web_uri') + "/accounts/" + params.account_id +  "/refunds/" + params.refund_id + "?embedded=1";
-  }
+  model: model
 });
 
 Balanced.AccountsHoldRoute = Balanced.AuthRoute.extend({
-  model: function(params) {
-    var marketplace = this.modelFor('marketplace');
-    return marketplace.get('web_uri') + "/accounts/" + params.account_id +  "/holds/" + params.hold_id + "?embedded=1";
-  }
+  model: model,
+
 });
 
 ////
 // Aliases
 //
-// These are not quite working yet. Still need to be fixed.
 ////
 Balanced.MarketplaceCreditsRoute = Balanced.AuthRoute.extend({
-  redirect: function () {
-    this.transitionTo('accounts.credit');
+  model: model,
+  renderTemplate: function() {
+    this.render('accounts/credit');
   }
 });
 
 Balanced.MarketplaceDebitsRoute = Balanced.AuthRoute.extend({
-  redirect: function () {
-    this.transitionTo('accounts.debit');
+  model: model,
+  renderTemplate: function() {
+    this.render('accounts/debit');
   }
 });
 
 Balanced.MarketplaceRefundsRoute = Balanced.AuthRoute.extend({
-  redirect: function () {
-    this.transitionTo('accounts.refund');
+  model: model,
+  renderTemplate: function() {
+    this.render('accounts/refund');
   }
 });
 
 Balanced.MarketplaceHoldsRoute = Balanced.AuthRoute.extend({
-  redirect: function () {
-    this.transitionTo('accounts.hold');
+  model: model,
+  renderTemplate: function() {
+    this.render('accounts/hold');
   }
 });
