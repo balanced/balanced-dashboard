0807d26c51ca3ad7a9fe9c93a7095dbbf519d90f
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-05-15 16:24:16 -0700

    Fix issue clicking marketplace link

--- app/routes/marketplace_index.js
+++ app/routes/marketplace_index.js
@@ -1,5 +1,12 @@
 Balanced.MarketplaceIndexRoute = Balanced.AuthRoute.extend({
   model: function(params) {
     return this.modelFor('marketplace');
+  },
+
+  // if we passed a lite marketplace to #linkTo, need this to find the real marketplace
+  setupController: function(controller, model) {
+    if(model._type === "marketplaceLite") {
+      controller.set("content", Balanced.Marketplace.find(model.uri));
+    }
   }
 });
