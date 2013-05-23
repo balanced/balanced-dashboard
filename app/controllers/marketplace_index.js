13fcc0f943ec0ef032b613ba39b203292dc9fb02
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-05-22 19:30:30 -0700

    Add loading spinner while data for settings is loaded

--- app/controllers/marketplace_index.js
+++ app/controllers/marketplace_index.js
@@ -1,6 +1,23 @@
 Balanced.MarketplaceIndexController = Balanced.ObjectController.extend({
   needs: ["marketplace"],
 
+  fullyLoaded: function() {
+    var ownerAccount = this.get('owner_account');
+    return this.get('isLoaded')
+      && this.get('callbacks').get('isLoaded')
+      && ownerAccount.get('isLoaded')
+      && ownerAccount.get('customer').get('isLoaded')
+      && ownerAccount.get('bank_accounts').get('isLoaded')
+      && ownerAccount.get('cards').get('isLoaded');
+  }.property(
+    'isLoaded',
+    'callbacks.isLoaded',
+    'owner_account.isLoaded',
+    'owner_account.customer.isLoaded',
+    'owner_account.bank_accounts.isLoaded',
+    'owner_account.cards.isLoaded'
+    ),
+
   deleteCard: function(card) {
   	card.delete();
   },
