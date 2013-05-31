843d9d79c4071d8fcb46dfe27d8c15e9d941741b
Justin <justin@nodesocket.com>
2013-05-31 15:17:56 -0700

    Add funds and withdraw funds now only allows verified bank accounts. Also does not break if no bank account are verified.

--- app/models/customer.js
+++ app/models/customer.js
@@ -1,4 +1,18 @@
 Balanced.Customer = Balanced.Model.extend({
 	bank_accounts: Balanced.Model.hasMany('Balanced.BankAccount', 'bank_accounts_uri'),
+
+    verified_bank_accounts: function() {
+        var bank_accounts = this.get('bank_accounts');
+
+        valid_bank_accounts = [];
+        for(var i = 0; i < bank_accounts.length; i++) {
+            if(bank_accounts[i].get('verified')) {
+                valid_bank_accounts.push(bank_accounts[i]);
+            }
+        }
+
+        return valid_bank_accounts;
+    }.property('bank_accounts'),
+
     cards: Balanced.Model.hasMany('Balanced.Card', 'cards_uri')
 });
