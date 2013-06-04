af71ae8c78532446891ee96978919708e9e2854f
Marshall Jones <marshall@balancedpayments.com>
2013-06-04 10:30:19 -0700

    only log errors to sentry if not in debug mode.

--- app/lib/errors.js
+++ app/lib/errors.js
@@ -2,6 +2,9 @@ if (typeof Raven !== typeof undefined) {
     Raven.config('https://c5e331a1bd9c47af85d481e46b415dab@app.getsentry.com/6353').install();
 
     Ember.onerror = function (error) {
-        Raven.captureException(error);
+        if (!ENV.BALANCED.DEBUG) {
+            Raven.captureException(error);
+        }
+        Ember.Logger.error(error.message);
     };
 }
