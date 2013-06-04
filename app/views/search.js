648c4abba1fefba99f72f53dc886ef45a43db158
Marshall Jones <marshall@balancedpayments.com>
2013-06-04 10:58:44 -0700

    formatting

--- app/views/search.js
+++ app/views/search.js
@@ -2,233 +2,233 @@
 Balanced.THROTTLE = 400;
 
 Balanced.SearchView = Balanced.View.extend({
-  templateName: 'search',
+    templateName: 'search',
 
-  resultsClass: 'with-results',
-  sorts: ['unsorted', 'ascending', 'descending'],
+    resultsClass: 'with-results',
+    sorts: ['unsorted', 'ascending', 'descending'],
 
-  // TODO - Do we need these here? Seems like they're mirroring the controller...
-  sortOrder: null,
+    // TODO - Do we need these here? Seems like they're mirroring the controller...
+    sortOrder: null,
 
-  didInsertElement: function() {
-    $(document).on('click', $.proxy(this.shouldCloseSearch, this));
-  },
+    didInsertElement: function () {
+        $(document).on('click', $.proxy(this.shouldCloseSearch, this));
+    },
 
-  selectSearchResult: function(uri) {
-    this.reset();
-    this.get('controller').send('selectSearchResult', uri);
-  },
+    selectSearchResult: function (uri) {
+        this.reset();
+        this.get('controller').send('selectSearchResult', uri);
+    },
 
-  closeSearch: function() {
-    this.reset();
-  },
+    closeSearch: function () {
+        this.reset();
+    },
 
-  shouldCloseSearch: function(e) {
-      var $target = $(e.target);
-      // sometimes ember likes to remove nodes from the dom when you click on
-      // them so the body check will make sure it's legit.
-      if ($target.closest('#search').length === 0 && $target.closest('body').length > 0) {
-        $('#search').removeClass(this.resultsClass);
+    shouldCloseSearch: function (e) {
+        var $target = $(e.target);
+        // sometimes ember likes to remove nodes from the dom when you click on
+        // them so the body check will make sure it's legit.
+        if ($target.closest('#search').length === 0 && $target.closest('body').length > 0) {
+            $('#search').removeClass(this.resultsClass);
+            $("body").removeClass("overlaid");
+        }
+    },
+
+    reset: function () {
+        var $q = $('#q');
+        var $searchArea = $('#search');
+        $searchArea.removeClass(this.resultsClass);
+        $q.val('').focus();
+
         $("body").removeClass("overlaid");
-      }
-  },
+        this.resetHeader();
+    },
 
-  reset: function() {
-    var $q = $('#q');
-    var $searchArea = $('#search');
-    $searchArea.removeClass(this.resultsClass);
-    $q.val('').focus();
+    resetHeader: function () {
+        this.resetSortOrder();
+        this.resetDateTimePicker();
+    },
 
-    $("body").removeClass("overlaid");
-    this.resetHeader();
-  },
+    resetSortOrder: function () {
+        var allSorts = $('#search .sortable');
+        allSorts.removeClass(this.sorts.join(' '));
+    },
 
-  resetHeader: function() {
-    this.resetSortOrder();
-    this.resetDateTimePicker();
-  },
+    resetDateTimePicker: function () {
+        var $dps = $('#search .dp'),
+            $sets = $('#search .set-times li');
 
-  resetSortOrder: function() {
-    var allSorts = $('#search .sortable');
-    allSorts.removeClass(this.sorts.join(' '));
-  },
+        $sets.removeClass('selected');
+        $dps.val('');
+        $('#search .timing > .dropdown-toggle > span').text('Any time');
+    },
 
-  resetDateTimePicker: function() {
-    var $dps = $('#search .dp'),
-      $sets = $('#search .set-times li');
+    onQueryChange: function (e) {
+        var self = this;
 
-    $sets.removeClass('selected');
-    $dps.val('');
-    $('#search .timing > .dropdown-toggle > span').text('Any time');
-  },
+        if ($("#q").val().length === 0) {
+            self.toggleResults();
+            return;
+        }
 
-  onQueryChange: function(e) {
-    var self = this;
+        self._runSearch(function () {
+            self.toggleResults();
+            self._highlightResults();
+        });
+    },
 
-    if($("#q").val().length === 0) {
-      self.toggleResults();
-      return;
+    onSortChange: function (e, field) {
+        var $t = $(e.currentTarget);
+        var sequences = {};
+        this.resetSortOrder();
+        for (var i = 0; i < this.sorts.length; i++) {
+            sequences[this.sorts[i]] = this.sorts[(i + 1) % this.sorts.length];
+        }
+        var currentSort = $t.data('direction') || 'unsorted';
+        var nextSort = sequences[currentSort];
+        $t.data('direction', nextSort).addClass(nextSort);
+
+        var mappedSortOrder = "none";
+        switch (nextSort) {
+            case "ascending":
+                mappedSortOrder = "asc";
+                break;
+            case "descending":
+                mappedSortOrder = "desc";
+                break;
+        }
+
+        this._setSortOrder(field, mappedSortOrder);
+    },
+
+    onChangeSearchType: function (e, searchType) {
+        var $t = $(e.currentTarget);
+
+        ////
+        // Don't re-run query if already on selected
+        ////
+        if ($t.closest('li').hasClass("selected")) {
+            return;
+        }
+
+        var typeToClass = {
+            'transaction': 'transactions',
+            'account': 'accounts',
+            'funding_instrument': 'funding-instruments'
+        };
+        var typeToSelect = typeToClass[searchType] || searchType;
+
+        $t.closest('nav').find(' > li').removeClass('selected');
+        $t.closest('li').addClass('selected');
+        $('#search .items').removeClass('selected');
+        $('#search .items.' + typeToSelect).addClass('selected');
+
+        this.get('controller').send('changeTypeFilter', searchType);
+        this._runSearch();
+    },
+
+    filterResultType: function (e, filter, label) {
+        var $t = $(e.currentTarget);
+
+        $t.parents("nav").find("li.selected").removeClass("selected");
+        $t.parents("li.filter").addClass("selected");
+
+        ////
+        // Switch to the correct items table
+        ////
+        if ($t.parents("nav li.filter").hasClass("transactions")) {
+            $('#search .items').removeClass('selected');
+            $('#search .items.transactions').addClass('selected');
+        } else if ($t.parents("nav li.filter").hasClass("funding-instruments")) {
+            $('#search .items').removeClass('selected');
+            $('#search .items.funding-instruments').addClass('selected');
+        }
+
+        this.get('controller').send('changeTypeFilter', filter);
+        this._runSearch();
+    },
+
+    toggleResults: function () {
+        var $q = $('#q');
+        var $searchArea = $('#search');
+        var fn = $q.val() ? $searchArea.addClass : $searchArea.removeClass;
+
+        fn.call($searchArea, this.resultsClass);
+
+        if ($q.val()) {
+            $("body").addClass("overlaid");
+        } else {
+            $("body").removeClass("overlaid");
+        }
+    },
+
+    _highlightResults: function () {
+        var query = $('#q').val();
+
+        //  remove empty words
+        $('#search .results tbody').highlightWords(query);
+    },
+
+    _setSortOrder: function (field, sortOrder) {
+        this.get("controller").send("changeSortOrder", field, sortOrder);
+    },
+
+    _runSearch: function (callback) {
+        this.get('controller').send('query', callback);
     }
-
-    self._runSearch(function() {
-      self.toggleResults();
-      self._highlightResults();
-    });
-  },
-
-  onSortChange: function(e, field) {
-    var $t = $(e.currentTarget);
-    var sequences = {};
-    this.resetSortOrder();
-    for (var i = 0; i < this.sorts.length; i++) {
-      sequences[this.sorts[i]] = this.sorts[(i + 1) % this.sorts.length];
-    }
-    var currentSort = $t.data('direction') || 'unsorted';
-    var nextSort = sequences[currentSort];
-    $t.data('direction', nextSort).addClass(nextSort);
-
-    var mappedSortOrder = "none";
-    switch(nextSort) {
-      case "ascending":
-        mappedSortOrder = "asc";
-        break;
-      case "descending":
-        mappedSortOrder = "desc";
-        break;
-    }
-
-    this._setSortOrder(field, mappedSortOrder);
-  },
-
-  onChangeSearchType: function(e, searchType) {
-    var $t = $(e.currentTarget);
-
-    ////
-    // Don't re-run query if already on selected
-    ////
-    if($t.closest('li').hasClass("selected")) {
-      return;
-    }
-
-    var typeToClass = {
-        'transaction': 'transactions',
-        'account': 'accounts',
-        'funding_instrument': 'funding-instruments'
-    };
-    var typeToSelect = typeToClass[searchType] || searchType;
-
-    $t.closest('nav').find(' > li').removeClass('selected');
-    $t.closest('li').addClass('selected');
-    $('#search .items').removeClass('selected');
-    $('#search .items.' + typeToSelect).addClass('selected');
-
-    this.get('controller').send('changeTypeFilter', searchType);
-    this._runSearch();
-  },
-
-  filterResultType: function(e, filter, label) {
-    var $t = $(e.currentTarget);
-
-    $t.parents("nav").find("li.selected").removeClass("selected");
-    $t.parents("li.filter").addClass("selected");
-
-    ////
-    // Switch to the correct items table
-    ////
-    if($t.parents("nav li.filter").hasClass("transactions")) {
-      $('#search .items').removeClass('selected');
-      $('#search .items.transactions').addClass('selected');
-    } else if($t.parents("nav li.filter").hasClass("funding-instruments")) {
-      $('#search .items').removeClass('selected');
-      $('#search .items.funding-instruments').addClass('selected');
-    }
-
-    this.get('controller').send('changeTypeFilter', filter);
-    this._runSearch();
-  },
-
-  toggleResults: function() {
-    var $q = $('#q');
-    var $searchArea = $('#search');
-    var fn = $q.val() ? $searchArea.addClass : $searchArea.removeClass;
-
-    fn.call($searchArea, this.resultsClass);
-
-    if($q.val()) {
-      $("body").addClass("overlaid");
-    } else {
-      $("body").removeClass("overlaid");
-    }
-  },
-
-  _highlightResults: function() {
-    var query = $('#q').val();
-
-    //  remove empty words
-    $('#search .results tbody').highlightWords(query);
-  },
-
-  _setSortOrder: function(field, sortOrder) {
-    this.get("controller").send("changeSortOrder", field, sortOrder);
-  },
-
-  _runSearch: function(callback) {
-    this.get('controller').send('query', callback);
-  }
 });
 
 Balanced.SearchQueryInputView = Balanced.Forms.TextField.extend({
-  attributeBindings: ['autocomplete'],
+    attributeBindings: ['autocomplete'],
 
-  didInsertElement: function() {
-    this.keyUp = _.throttle(function(e) {
-      // Hide search results on escape key
-      if(e.keyCode === Balanced.KEYS.ESCAPE) {
-        $("#search").removeClass(this.get('parentView').resultsClass);
-        $("body").removeClass("overlaid");
-        return;
-      }
+    didInsertElement: function () {
+        this.keyUp = _.throttle(function (e) {
+            // Hide search results on escape key
+            if (e.keyCode === Balanced.KEYS.ESCAPE) {
+                $("#search").removeClass(this.get('parentView').resultsClass);
+                $("body").removeClass("overlaid");
+                return;
+            }
 
-      this.get('parentView').onQueryChange(e);
+            this.get('parentView').onQueryChange(e);
 
-    }, Balanced.THROTTLE);
-  },
+        }, Balanced.THROTTLE);
+    },
 
-  focusIn: function(e) {
-    $('#search').addClass('focus');
-  },
+    focusIn: function (e) {
+        $('#search').addClass('focus');
+    },
 
-  focusOut: function(e) {
-    $('#search').removeClass('focus');
-  },
+    focusOut: function (e) {
+        $('#search').removeClass('focus');
+    },
 });
 
 Balanced.SearchSortableColumnHeaderView = Balanced.View.extend({
-  tagName: 'th',
+    tagName: 'th',
 
-  click: function(e) {
-    this.get('parentView').onSortChange(e, this.field);
-  }
+    click: function (e) {
+        this.get('parentView').onSortChange(e, this.field);
+    }
 });
 
 Balanced.SearchTypeView = Balanced.View.extend({
-  tagName: 'a',
-  attributeBindings: ['href'],
-  href: '#',
+    tagName: 'a',
+    attributeBindings: ['href'],
+    href: '#',
 
-  click: function(e) {
-    e.preventDefault();
-    this.get('parentView').onChangeSearchType(e, this.searchType);
-  }
+    click: function (e) {
+        e.preventDefault();
+        this.get('parentView').onChangeSearchType(e, this.searchType);
+    }
 });
 
 Balanced.SearchFilterResultView = Balanced.View.extend({
-  tagName: 'a',
-  attributeBindings: ['href'],
-  href: '#',
+    tagName: 'a',
+    attributeBindings: ['href'],
+    href: '#',
 
-  click: function(e) {
-    e.preventDefault();
-    this.get('parentView').filterResultType(e, this.filter, this.label);
-  }
+    click: function (e) {
+        e.preventDefault();
+        this.get('parentView').filterResultType(e, this.filter, this.label);
+    }
 });
