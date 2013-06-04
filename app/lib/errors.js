539d16fa22f0e3ee67e161a325ad719ff9dc4b8a
Marshall Jones <marshall@balancedpayments.com>
2013-06-04 10:19:54 -0700

    add sentry error handling to fix #61

---
+++ app/lib/errors.js
@@ -0,0 +1,7 @@
+if (typeof Raven !== typeof undefined) {
+    Raven.config('https://c5e331a1bd9c47af85d481e46b415dab@app.getsentry.com/6353').install();
+
+    Ember.onerror = function (error) {
+        Raven.captureException(error);
+    };
+}
