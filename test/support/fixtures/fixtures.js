860ca43201a0ff01ce8e73715aace5d637877cdb
Marshall Jones <marshall@balancedpayments.com>
2013-06-05 18:23:13 -0700

    stop using MarketplaceLite, always use full Marketplace obj

--- test/support/fixtures/fixtures.js
+++ test/support/fixtures/fixtures.js
@@ -1,8 +1,8 @@
-window.setupTestFixtures = function() {
+window.setupTestFixtures = function () {
 
-require('test/support/fixtures/customer');
-require('test/support/fixtures/marketplace');
-require('test/support/fixtures/search_query');
-require('test/support/fixtures/user');
+    require('test/support/fixtures/customer');
+    require('test/support/fixtures/marketplace');
+    require('test/support/fixtures/search_query');
+    require('test/support/fixtures/user');
 
 };
