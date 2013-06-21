94ebc2c69405db93b8cc574cc13c4820193633cc
Marshall Jones <marshall@balancedpayments.com>
2013-06-20 17:38:25 -0700

    start of the start

--- app/routes/start.js
+++ app/routes/start.js
@@ -1,23 +1,35 @@
 Balanced.StartRoute = Balanced.Route.extend({
-    redirect: function () {
-        var self = this;
-        if (Balanced.Auth.get('user') && !Balanced.Auth.get('isGuest')) {
-            self.transitionTo('index');
-            return;
-        }
-        Balanced.Model.create({
-            // TODO: get this from the api
-            uri: '/v1/api_keys'
-        }).create().then(function (apiKey) {
-            Balanced.Auth.storeGuestAPIKey(apiKey.secret);
-
-            Balanced.Marketplace.create({
+    model: function () {
+        var apiKey = Balanced.Model.create({
+                // TODO: get this from the api
+                uri: '/v1/api_keys'
+            }),
+            marketplace = Balanced.Marketplace.create({
                 uri: '/v1/marketplaces'
-            }).create().then(function (marketplace) {
+            });
+        var models = {
+            apiKey: apiKey,
+            marketplace: marketplace
+        };
+        apiKey.create().then(function (apiKey) {
+            Balanced.Auth.storeGuestAPIKey(apiKey.secret);
+            marketplace.then(function (marketplace) {
                 var marketplaces = Balanced.Auth.get('user').get('marketplaces');
                 marketplaces.pushObject(Balanced.Marketplace.create(marketplace));
-                self.transitionTo('marketplace.transactions', marketplace);
             });
         });
+        return models;
+    },
+    events: {
+        goToDashboard: function () {
+            console.log(this.currentModel.marketplace);
+            this.transitionTo('marketplace.index', this.currentModel.marketplace);
+        },
+        goToDocumentation: function () {
+            window.location = 'https://docs.balancedpayments.com';
+        },
+        goToApply: function () {
+            this.transitionTo('marketplaces.apply');
+        }
     }
 });
