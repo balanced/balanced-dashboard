26c4b4c1e6217411f17f25b4e7f5b5710587682a
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-06-27 17:11:19 -0700

    Fix naming issue with marketplace settings

--- app/dashboard.js
+++ app/dashboard.js
@@ -70,7 +70,7 @@ require('app/views/search');
 require('app/views/welcome_transition');
 
 require('app/views/marketplace/marketplace');
-require('app/views/marketplace/marketplace_index');
+require('app/views/marketplace/marketplace_settings');
 require('app/views/marketplace/add_bank_account_modal');
 require('app/views/marketplace/add_callback_modal');
 require('app/views/marketplace/add_card_modal');
