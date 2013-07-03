324cfc0f82af7dc4c166110d4868bc3aac4d22d1
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-07-02 17:30:39 -0700

    set current marketplace during model hook to prevent auth errors

--- app/routes/marketplaces/show.js
+++ app/routes/marketplaces/show.js
@@ -2,7 +2,9 @@ Balanced.MarketplaceRoute = Balanced.AuthRoute.extend({
     model: function (params) {
         var marketplaceURI = Balanced.Marketplace.constructUri(params.marketplace_id);
         balanced.init(marketplaceURI);
-        return Balanced.Marketplace.find(marketplaceURI);
+        var marketplace = Balanced.Marketplace.find(marketplaceURI);
+        Balanced.Utils.setCurrentMarketplace(marketplace);
+        return marketplace;
     },
 
     // if we passed a lite marketplace to #linkTo, need this to find the real marketplace
