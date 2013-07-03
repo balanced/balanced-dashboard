281ab08e8a4bab03858665d896c75006ad7e8e27
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-07-02 17:56:13 -0700

    Don't throw away marketplace lites when logging in

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,14 @@
 Balanced.User = Balanced.Model.extend({
 
+    marketplace_lites: Balanced.Model.hasMany('Balanced.MarketplaceLite', 'marketplace_lites'),
+
+    marketplaces: function() {
+        var marketplaceLites = this.get('marketplace_lites').get('content');
+        return _.map(marketplaceLites, function (marketplace) {
+            return Balanced.Marketplace.find(marketplace.uri);
+        });
+    }.property('marketplace_lites'),
+
     gravatar: function () {
         var emailHash = this.get('email_hash');
         return Balanced.Utils.toGravatar(emailHash);
@@ -9,15 +18,14 @@ Balanced.User = Balanced.Model.extend({
     deserialize: function (json) {
         this._super(json);
 
-        json.marketplaces = json.marketplaces || [];
-        json.marketplaces.sort(function (a, b) {
+        json.marketplace_lites = json.marketplaces || [];
+        delete json.marketplaces;
+
+        json.marketplace_lites.sort(function (a, b) {
             if (a.name === b.name) {
                 return 0;
             }
             return (a.name > b.name) ? 1 : -1;
-        });
-        json.marketplaces = _.map(json.marketplaces, function (marketplace) {
-            return Balanced.Marketplace.find(marketplace.uri);
         });
     }
 });
