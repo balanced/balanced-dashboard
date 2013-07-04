fed86f7bc878f0288827166b054ae5372398d837
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-07-03 18:31:33 -0700

    Simplify deserialization for search queries

--- app/models/search_query.js
+++ app/models/search_query.js
@@ -6,32 +6,57 @@ Balanced.SearchQuery = Balanced.Model.extend({
     accounts: Balanced.Model.hasMany('Balanced.Account', 'accounts'),
     funding_instruments: Balanced.Model.hasMany('Balanced.FundingInstrument', 'funding_instruments'),
 
+    total_credits: function() {
+        return this.get('counts.credit');
+    }.property('counts.credit'),
+
+    total_debits: function() {
+        return this.get('counts.debit');
+    }.property('counts.debit'),
+
+    total_holds: function() {
+        return this.get('counts.hold');
+    }.property('counts.hold'),
+
+    total_refunds: function() {
+        return this.get('counts.refund');
+    }.property('counts.refund'),
+
+    total_accounts: function() {
+        return this.get('counts.account');
+    }.property('counts.account'),
+
+    total_bank_accounts: function() {
+        return this.get('counts.bank_account');
+    }.property('counts.bank_account'),
+
+    total_cards: function() {
+        return this.get('counts.card');
+    }.property('counts.card'),
+
+    total_transactions: function() {
+        return this.get('total_credits') + this.get('total_debits') + this.get('total_holds') + this.get('total_refunds');
+    }.property('total_credits', 'total_debits', 'total_holds', 'total_refunds'),
+
+    total_funding_instruments: function() {
+        return this.get('total_bank_accounts') + this.get('total_cards');
+    }.property('total_bank_accounts', 'total_cards'),
+
     deserialize: function (json) {
         this._super(json);
 
-        var accountMap = {
-                'account': Balanced.Account
-            },
-            transactionMap = {
-                'credit': Balanced.Credit,
-                'debit': Balanced.Debit,
-                'refund': Balanced.Refund,
-                'hold': Balanced.Hold
-            },
-            fundingInstrumentMap = {
-                'card': Balanced.Card,
-                'bank_account': Balanced.BankAccount
-            };
+        var transactionTypes = ['credit', 'debit', 'refund', 'hold'];
+        var fundingInstrumentTypes = ['card', 'bank_account'];
 
-        function itemFilter(map) {
+        function itemFilter(typesArray) {
             return function (item) {
-                return Object.keys(map).indexOf(item._type) > -1;
+                return _.contains(typesArray, item._type);
             };
         }
 
-        var transactions = _.chain(json.items).filter(itemFilter(transactionMap)).value();
-        var accounts = _.chain(json.items).filter(itemFilter(accountMap)).value();
-        var funding_instruments = _.chain(json.items).filter(itemFilter(fundingInstrumentMap)).value();
+        var transactions = _.filter(json.items, itemFilter(transactionTypes));
+        var accounts = _.filter(json.items, itemFilter(['account']));
+        var funding_instruments = _.filter(json.items, itemFilter(fundingInstrumentTypes));
 
         function constructResults(items, json) {
             var results = {
@@ -45,19 +70,6 @@ Balanced.SearchQuery = Balanced.Model.extend({
         json.transactions = constructResults(transactions, json);
         json.accounts = constructResults(accounts, json);
         json.funding_instruments = constructResults(funding_instruments, json);
-
-        json.total_credits = json.counts.credit;
-        json.total_debits = json.counts.debit;
-        json.total_holds = json.counts.hold;
-        json.total_refunds = json.counts.refund;
-
-        json.total_accounts = json.counts.account;
-
-        json.total_bank_accounts = json.counts.bank_account;
-        json.total_cards = json.counts.card;
-
-        json.total_transactions = json.total_credits + json.total_debits + json.total_holds + json.total_refunds;
-        json.total_funding_instruments = json.total_bank_accounts + json.total_cards;
     }
 });
 
