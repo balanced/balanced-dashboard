ed4cc0245067baee4a12244f2eca454804c02434
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-07-03 19:32:36 -0700

    Organize search controller

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -28,7 +28,7 @@ Balanced.SearchController = Balanced.ObjectController.extend(Balanced.DownloadCo
         }
 
         var self = this;
-        var params = this.searchParams({
+        var params = this._searchParams({
             query: query
         });
 
@@ -46,64 +46,8 @@ Balanced.SearchController = Balanced.ObjectController.extend(Balanced.DownloadCo
         return current && !current.get('isLoaded');
     }.property('loading_content.isLoaded'),
 
-    getLabel: function (labelMapping, acceptedTypes, type) {
-        var label = labelMapping[type];
-        if (!label && acceptedTypes.indexOf(type) > -1) {
-            label = Balanced.Utils.toTitleCase(type.replace('_', ' ')) + 's';
-        }
-        return (label) ? label : labelMapping.DEFAULT;
-    },
-
-    transaction_type: function () {
-        var types = ['debit', 'credit', 'hold', 'refund', 'transactions'];
-        return types.indexOf(this.type) > -1 ? 'transactions' : null;
-    }.property('content.type'),
-
-    transaction_type_label: function () {
-        var typesToLabels = {
-            DEFAULT: 'Transactions'
-        };
-        var types = ['debit', 'credit', 'hold', 'refund'];
-        return this.getLabel(typesToLabels, types, this.type);
-    }.property('content.type'),
-
-    transaction_type_total: function () {
-        var types = ['debit', 'credit', 'hold', 'refund'];
-        var type = this.get('type');
-        return (types.indexOf(type) >= 0 && this.get('total_{0}s'.format(type))) || this.get('total_transactions');
-    }.property('content.uri'),
-
-    funding_instrument_type_label: function () {
-        var typesToLabels = {
-            DEFAULT: 'Cards & Bank Accounts'
-        };
-        var types = ['bank_account', 'card'];
-        return this.getLabel(typesToLabels, types, this.type);
-    }.property('content.type'),
-
-    getSearchUri: function () {
-        var query = this.get('search');
-        var marketplaceUri = this.get('controllers').get('marketplace').get('uri');
-        var params = this.searchParams({
-            query: query
-        });
-        return Balanced.SearchQuery.createUri(marketplaceUri, params);
-    },
-
     loadMore: function(results) {
         results.loadNextPage();
-    },
-
-    searchParams: function (params) {
-        var defaults = {
-            limit: this.get('limit'),
-            minDate: this.get('minDate'),
-            maxDate: this.get('maxDate'),
-            sortField: this.get('sortField'),
-            sortOrder: this.get('sortOrder'),
-            type: this.get('type')
-        };
-        return $.extend({}, defaults, params);
     },
 
     reset: function () {
@@ -131,6 +75,16 @@ Balanced.SearchController = Balanced.ObjectController.extend(Balanced.DownloadCo
         this.set('type', type || 'transactions');
     },
 
+    // For download mixin
+    getSearchUri: function () {
+        var query = this.get('search');
+        var marketplaceUri = this.get('controllers').get('marketplace').get('uri');
+        var params = this._searchParams({
+            query: query
+        });
+        return Balanced.SearchQuery.createUri(marketplaceUri, params);
+    },
+
     selectSearchResult: function (uri) {
         window.location.hash = '#' + Balanced.Utils.uriToDashboardFragment(uri);
     },
@@ -141,6 +95,30 @@ Balanced.SearchController = Balanced.ObjectController.extend(Balanced.DownloadCo
             ohm
         );
     },
+
+    // UI computed properties
+
+    transaction_type_label: function () {
+        var typesToLabels = {
+            DEFAULT: 'Transactions'
+        };
+        var types = ['debit', 'credit', 'hold', 'refund'];
+        return this._getLabel(typesToLabels, types, this.type);
+    }.property('content.type'),
+
+    transaction_type_total: function () {
+        var types = ['debit', 'credit', 'hold', 'refund'];
+        var type = this.get('type');
+        return (types.indexOf(type) >= 0 && this.get('total_{0}s'.format(type))) || this.get('total_transactions');
+    }.property('content.uri'),
+
+    funding_instrument_type_label: function () {
+        var typesToLabels = {
+            DEFAULT: 'Cards & Bank Accounts'
+        };
+        var types = ['bank_account', 'card'];
+        return this._getLabel(typesToLabels, types, this.type);
+    }.property('content.type'),
 
     totalTransactionsHeader: function () {
         if (this.get('content')) {
@@ -158,5 +136,27 @@ Balanced.SearchController = Balanced.ObjectController.extend(Balanced.DownloadCo
             return 'Cards & Bank Accounts (0)';
         }
 
-    }.property('content.total_funding_instruments')
+    }.property('content.total_funding_instruments'),
+
+    // internal functions
+
+    _searchParams: function (params) {
+        var defaults = {
+            limit: this.get('limit'),
+            minDate: this.get('minDate'),
+            maxDate: this.get('maxDate'),
+            sortField: this.get('sortField'),
+            sortOrder: this.get('sortOrder'),
+            type: this.get('type')
+        };
+        return $.extend({}, defaults, params);
+    },
+
+    _getLabel: function (labelMapping, acceptedTypes, type) {
+        var label = labelMapping[type];
+        if (!label && acceptedTypes.indexOf(type) > -1) {
+            label = Balanced.Utils.toTitleCase(type.replace('_', ' ')) + 's';
+        }
+        return (label) ? label : labelMapping.DEFAULT;
+    }
 });
