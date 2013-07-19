23df3a5db7d25abf21b47af867715e9a25c79833
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-07-19 14:50:50 -0700

    jshint

--- app/routes/customers.js
+++ app/routes/customers.js
@@ -2,11 +2,11 @@ Balanced.CustomersRoute = Balanced.AuthRoute.extend({
 });
 
 Balanced.CustomerRoute = Balanced.AuthRoute.extend({
-    model: function (params) {
-    	var marketplace = this.modelFor('marketplace');
-        return marketplace.then(function(marketplace) {
-        	var customerUri = marketplace.get('customers_uri') + '/' + params.customer_id;
-        	return Balanced.Customer.find(customerUri);
-        });
-    }
+	model: function (params) {
+		var marketplace = this.modelFor('marketplace');
+		return marketplace.then(function(marketplace) {
+			var customerUri = marketplace.get('customers_uri') + '/' + params.customer_id;
+			return Balanced.Customer.find(customerUri);
+		});
+	}
 });
