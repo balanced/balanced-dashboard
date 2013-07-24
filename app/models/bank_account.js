4cb0b2bb0d32e391bbd1456b97083390ddd471f0
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-07-24 12:16:15 -0700

    Hide verification flow for orphaned bank accounts

--- app/models/bank_account.js
+++ app/models/bank_account.js
@@ -25,11 +25,17 @@ Balanced.BankAccount = Balanced.FundingInstrument.extend({
         return 'Bank account: {0}'.format(this.get('description'));
     }.property('description'),
 
+    can_verify: function() {
+        return !this.get('can_debit') &&
+                (this.get('customer') || this.get('account'));
+    }.property('can_debit', 'can_confirm_verification'),
+
     can_confirm_verification: function() {
-        return this.get('verification.state') !== 'failed' &&
-               this.get('verification.state') !== 'verified' &&
-               this.get('verification.remaining_attempts') > 0;
-    }.property('verification.state', 'verification.remaining_attempts')
+        return this.get('verification') &&
+                this.get('verification.state') !== 'failed' &&
+                this.get('verification.state') !== 'verified' &&
+                this.get('verification.remaining_attempts') > 0;
+    }.property('verification', 'verification.state', 'verification.remaining_attempts')
 });
 
 Balanced.TypeMappings.addTypeMapping('bank_account', 'Balanced.BankAccount');
