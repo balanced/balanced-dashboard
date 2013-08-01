83f1620b5336ae9b75d9eff5e9c3e0f9e9ae23da
Victor Lin <bornstub@gmail.com>
2013-08-01 09:21:20 +0800

    Add feature-activity-new-tab changes back

--- app/routes/application.js
+++ app/routes/application.js
@@ -28,36 +28,6 @@ Balanced.ApplicationRoute = Balanced.Route.extend({
                     withCredentials: true
                 }
             });
-        },
-
-        selectResult: function (obj) {
-            var self = this;
-            if (obj.constructor === Balanced.Account) {
-                var marketplace = this.modelFor('marketplace');
-                var accountId = obj.get('id');
-                obj = marketplace.then(function (marketplace) {
-                    var customerUri = marketplace.get('customers_uri') + '/' + accountId;
-                    self.transitionTo('customer', Balanced.Customer.find(customerUri));
-                });
-                return;
-            }
-
-            if (obj.constructor === Balanced.Customer) {
-                this.transitionTo('customer', obj);
-                return;
-            }
-
-            if (obj.constructor === Balanced.BankAccount) {
-                this.transitionTo('bank_account', obj);
-                return;
-            }
-
-            if (obj.constructor === Balanced.Card) {
-                this.transitionTo('card', obj);
-                return;
-            }
-
-            window.location.hash = '#' + Balanced.Utils.uriToDashboardFragment(obj.uri);
-        },
+        }
     }
 });
