3d1810e908dd63c09e36d21ded803d2e79a3bced
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-08-06 17:20:50 -0700

    jshint

--- app/lib/errors.js
+++ app/lib/errors.js
@@ -2,7 +2,7 @@ if (typeof Raven !== typeof undefined) {
     Raven.config('https://c5e331a1bd9c47af85d481e46b415dab@app.getsentry.com/6353').install();
 
     Ember.onerror = function (error) {
-    	var realError = error.stack || error;
+        var realError = error.stack || error;
 
         if (!ENV.BALANCED.DEBUG) {
             Raven.captureException(realError);
