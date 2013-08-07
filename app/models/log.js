19b0f54696cebdd7db02efb396d0492ea8aaaf3c
Justin Keller <hello@nodesocket.com>
2013-08-07 16:56:36 -0700

    Align right date in logs. Removed un-needed computed prop in logs model.

--- app/models/log.js
+++ app/models/log.js
@@ -7,14 +7,6 @@ Balanced.Log = Balanced.Model.extend({
         return Balanced.Utils.prettyLogUrl(this.get('short_url'));
     }.property('log.short_url'),
 
-    human_readable_date: function() {
-        if (this.get('created_at')) {
-            return Date.parseISO8601(this.get('created_at')).strftime(this.date_formats.short);
-        } else {
-            return '';
-        }
-    }.property('log.created_at'),
-
     geo_ip: function() {
         var ip = this.get('message.request.headers.X-Real-Ip');
 
