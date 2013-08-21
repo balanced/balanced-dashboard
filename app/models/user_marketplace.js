8d7778fee3007908a05dccbea51a28c7b55fd522
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-08-21 11:23:05 -0700

    Add type mapping for UserMarketplace

--- app/models/user_marketplace.js
+++ app/models/user_marketplace.js
@@ -36,3 +36,5 @@ Balanced.UserMarketplace = Balanced.Model.extend({
         return this.get('uri').indexOf('TEST') === -1;
     }.property('uri')
 });
+
+Balanced.TypeMappings.addTypeMapping('user_marketplace', 'Balanced.UserMarketplace');
