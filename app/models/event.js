38f7071918b96d12be9124ca94f48c6c7692b255
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-08-28 13:42:28 -0700

    Add type mapping for events

--- app/models/event.js
+++ app/models/event.js
@@ -2,3 +2,5 @@ Balanced.Event = Balanced.Model.extend({
 	transaction:        Balanced.Model.belongsTo('entity','Balanced.Transaction'),
 	eventCallbacks:     Balanced.Model.hasMany('callbacks','Balanced.EventCallback')
 });
+
+Balanced.TypeMappings.addTypeMapping('event', 'Balanced.Event');
