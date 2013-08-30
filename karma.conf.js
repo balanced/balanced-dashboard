c75cad7f5e54f000ed78657aec836f65b8a7f178
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-08-29 18:15:44 -0700

    Switch to karma test runner

---
+++ karma.conf.js
@@ -0,0 +1,76 @@
+// Karma configuration
+// Generated on Thu Aug 29 2013 15:59:23 GMT-0700 (PDT)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+    // frameworks to use
+    frameworks: ['qunit'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'build/css/base.min.css',
+      'build/test/js/sinon.js',
+      'build/test/js/testenv.js',
+      'build/js/lib-prod.js',
+      'build/js/dashboard-prod.js',
+      'build/test/js/test-fixtures.js',
+      'test/support/testconfig.js',
+      'test/lib/helpers.js',
+      'test/unit/**/*',
+      'test/integration/**/*'
+    ],
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+    preprocessors: {
+      'build/js/dashboard-prod.js': ['coverage']
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    },
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+
+    // TravisCI only has Firefox
+    browsers: ['Firefox'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  });
+};
---
+++ test/support/testenv.js
@@ -0,0 +1,12 @@
+TESTING = true;
+var ENV = {
+    BALANCED: {
+        API: 'https://api.balancedpayments.com',
+        USER: 'https://auth.balancedpayments.com',
+        WWW: 'https://www.balancedpayments.com',
+        DOCS: 'https://docs.balancedpayments.com',
+        DEBUG: true,
+        MIXPANEL: '',
+        GOOGLE_ANALYTICS: ''
+    }
+};
