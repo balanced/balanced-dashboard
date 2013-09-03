040f8eae070a5a5aa3d0737265f0b6c82a390883
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-09-03 12:24:59 -0700

    Add page title for reversals

--- app/routes/reversals.js
+++ app/routes/reversals.js
@@ -1,6 +1,15 @@
 Balanced.ReversalsRoute = Balanced.AuthRoute.extend({
 	title: 'Reversal',
 
+	pageTitle: function (route, setTitle) {
+        var reversal = route.controller.content;
+        return Balanced.Utils.maybeDeferredLoading(reversal, setTitle, function () {
+            return 'Reversal: loading ...';
+        }, function () {
+            return 'Reversal: {0}'.format(reversal.get('page_title'));
+        });
+    },
+
 	model: function (params) {
 		var marketplace = this.modelFor('marketplace');
 		return marketplace.then(function(marketplace) {
