8a5f807b066adf26e16d58b28fdae834694783c1
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-09-03 15:46:00 -0700

    Invoice charges table

--- app/models/invoice.js
+++ app/models/invoice.js
@@ -12,7 +12,20 @@ Balanced.Invoice = Balanced.Model.extend({
 			return period;
 		}
 		return period[1];
-	}.property('period')
+	}.property('period'),
+
+	subtotal: function() {
+		var total = this.get('total_fee');
+		var adjustments = this.get('adjustments_total_fee');
+		if(Ember.none(total) || Ember.none(adjustments)) {
+			return undefined;
+		}
+		return total - adjustments;
+	}.property('total_fee', 'adjustments_total_fee'),
+
+	is_scheduled: function() {
+		return this.get('state') == 'scheduled';
+	}.property('state')
 });
 
 Balanced.TypeMappings.addTypeMapping('invoice', 'Balanced.Invoice');
