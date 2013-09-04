e25aff85403e9d3d5aafca70fc69448b85016d78
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-09-03 18:40:53 -0700

    Add test for invoice page

--- test/integration/invoices.js
+++ test/integration/invoices.js
@@ -17,3 +17,21 @@ test('can visit page', function (assert) {
 test('shows invoices list', function(assert) {
 	assert.equal($("#invoices table tbody tr").length, 2);
 });
+
+test('invoice detail page', function(assert) {
+	visit('/marketplaces/MP5m04ORxNlNDm1bB7nkcgSY/invoices/IVDOATjeyAPTJMJPnBR83uE')
+	.then(function() {
+		assert.equal($(".invoice-balance-due-box .amount").text().trim(), "$17.85");
+		assert.equal($(".hold-details-row .total").text().trim(), "$17.85");
+		assert.equal($(".card-debit-details-row .total").text().trim(), "$2.45");
+		assert.equal($(".bank-account-debit-details-row .total").text().trim(), "$0.00");
+		assert.equal($(".succeeded-credit-details-row .total").text().trim(), "$0.00");
+		assert.equal($(".failed-credit-details-row .total").text().trim(), "$0.00");
+		assert.equal($(".refund-details-row .total").text().trim(), "-$2.45");
+		assert.equal($(".reversal-details-row .total").text().trim(), "$0.00");
+		assert.equal($(".chargeback-details-row .total").text().trim(), "$0.00");
+		assert.equal($(".subtotal-row .total").text().trim(), "$17.85");
+		assert.equal($(".adjustments-row .total").text().trim(), "$0.00");
+		assert.equal($(".total-balance-row .total").text().trim(), "$17.85");
+	});
+});
