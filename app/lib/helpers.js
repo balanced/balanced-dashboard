5f8ce3ec8a1433ff245a48cf82a705617be23b45
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-09-06 13:35:41 -0700

    Remove unused date formatting code

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -143,34 +143,6 @@ if (!String.prototype.format) {
     };
 }
 
-Balanced.Helpers = (function () {
-    // example of how to parse and format iso8601 dates. will parse the
-    // specified format and then attach the original unparsed date as a title
-    // attribute. may not be needed since we don't render the time info on the
-    // server.
-    function parseDateTime() {
-        var $t = $(this);
-        var dt = $t.attr('datetime');
-        var format = $t.data('format');
-        if (!dt || dt.indexOf('T') === -1) {
-            return;
-        }
-        var x = Date.parseISO8601(dt).strftime(format);
-        if (x.toUpperCase().indexOf('UNDEFINED') === -1 && x.toUpperCase().indexOf('NAN') === -1) {
-            $t.text(x);
-            if (!$t.attr('title')) {
-                $t.attr('title', dt);
-            }
-        }
-    }
-
-    return {
-        init: function () {
-            $('time[data-format]').each(parseDateTime);
-        }
-    };
-})();
-
 Balanced.Utils = {
     uriToDashboardFragment: function (uri) {
         // have to strip off the API version
