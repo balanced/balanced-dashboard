d1079189aa2d80d8e1a8963b3dff6857a551dffb
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-09-18 17:46:54 -0700

    Fix mixins

--- app/controllers/mixins/results_table.js
+++ app/controllers/mixins/results_table.js
@@ -1,203 +1,205 @@
 Balanced.ResultsTable = Ember.Mixin.create({
-    needs: ['marketplace'],
+	needs: ['marketplace'],
 
-    type: 'transaction',
-    endpoint: null,
-    status_rollup: null,
+	type: 'transaction',
+	endpoint: null,
+	status_rollup: null,
 
-    minDate: null,
-    maxDate: null,
-    dateFilterTitle: 'Any time',
+	minDate: null,
+	maxDate: null,
+	dateFilterTitle: 'Any time',
 
-    // override this if you want to allow the results table to be able to sort by none
-    allowSortByNone: false,
+	// override this if you want to allow the results table to be able to sort by none
+	allowSortByNone: false,
 
-    sortField: null,
-    sortOrder: null,
+	sortField: null,
+	sortOrder: null,
 
-    // override this to true if you want to use the search API to fetch results
-    useSearch: false,
+	// override this to true if you want to use the search API to fetch results
+	useSearch: false,
 
-    // we use this so we can display counts without having them reset every time you filter
-    last_loaded_search_result: null,
+	// we use this so we can display counts without having them reset every time you filter
+	last_loaded_search_result: null,
 
-    // must be overridden to provide content if not using search
-    results_base_uri: null,
+	// must be overridden to provide content if not using search
+	results_base_uri: null,
 
-    // must be overridden for data picker to work
-    baseClassSelector: null,
+	// must be overridden for data picker to work
+	baseClassSelector: null,
 
-    // override this and return a hash to add query params
-    extra_filtering_params: null,
+	// override this and return a hash to add query params
+	extra_filtering_params: null,
 
-    // override this if you conditionally don't want to get results
-    fetch_results: true,
+	// override this if you conditionally don't want to get results
+	fetch_results: true,
 
-    results: function () {
-        if (!this.get('fetch_results')) {
-            return null;
-        }
+	actions: {
+		changeDateFilter: function (minDate, maxDate, title) {
+			this.setProperties({
+				minDate: minDate,
+				maxDate: maxDate,
+				dateFilterTitle: title
+			});
+		},
 
-        if (this.get('useSearch')) {
-            return this.get('search_result.' + this.get('category') + 's');
-        }
+		changeSortOrder: function (field, sortOrder) {
+			this.setProperties({
+				sortField: field,
+				sortOrder: sortOrder
+			});
+		},
 
-        return Balanced.ModelArray.newArrayLoadedFromUri(
-            this.get('results_uri'),
-            this.get('results_type')
-        );
-    }.property(
-            'fetch_results', 'useSearch', 'results_uri', 'results_type',
-            'type', 'endpoint', 'status_rollup', 'search_result.transactions',
-            'search_result.accounts', 'search_result.funding_instruments'),
+		changeTypeFilter: function (type) {
+			this.set('type', type);
+		},
 
-    search_result: function () {
-        var self = this;
-        if (this.get('useSearch')) {
-            var search = Balanced.SearchQuery.search(
-                this.get('controllers.marketplace.uri'),
-                this.get('search_params')
-            );
+		changeEndpointFiler: function (endpoint) {
+			var filteringParams = Ember.copy(this.get('extra_filtering_params'));
 
-            search.then(function (searchQuery) {
-                self.set('last_loaded_search_result', searchQuery);
-            });
+			if (endpoint) {
+				filteringParams.endpoint = endpoint;
+			} else {
+				delete filteringParams.endpoint;
+			}
 
-            return search;
-        }
+			this.set('extra_filtering_params', filteringParams);
+		},
 
-        return null;
+		changeStatusRollupFilter: function (statuses) {
+			var filteringParams = Ember.copy(this.get('extra_filtering_params'));
 
-    }.property('useSearch', 'controllers.marketplace.uri', 'search_params'),
+			if (statuses) {
+				var status_rollup = {
+					'status_rollup[in]': statuses
+				};
 
-    changeDateFilter: function (minDate, maxDate, title) {
-        this.setProperties({
-            minDate: minDate,
-            maxDate: maxDate,
-            dateFilterTitle: title
-        });
-    },
+				$.extend(filteringParams, status_rollup);
+			} else {
+				delete filteringParams['status_rollup[in]'];
+			}
 
-    changeSortOrder: function (field, sortOrder) {
-        this.setProperties({
-            sortField: field,
-            sortOrder: sortOrder
-        });
-    },
+			this.set('extra_filtering_params', filteringParams);
+		},
 
-    changeTypeFilter: function (type) {
-        this.set('type', type);
-    },
+		loadMore: function (results) {
+			results.loadNextPage();
+		},
 
-    changeEndpointFiler: function (endpoint) {
-        var filteringParams = Ember.copy(this.get('extra_filtering_params'));
+		reload: function () {
+			this.notifyPropertyChange('search_params');
+		},
+	},
 
-        if (endpoint) {
-            filteringParams.endpoint = endpoint;
-        } else {
-            delete filteringParams.endpoint;
-        }
+	results: function () {
+		if (!this.get('fetch_results')) {
+			return null;
+		}
 
-        this.set('extra_filtering_params', filteringParams);
-    },
+		if (this.get('useSearch')) {
+			return this.get('search_result.' + this.get('category') + 's');
+		}
 
-    changeStatusRollupFilter: function (statuses) {
-        var filteringParams = Ember.copy(this.get('extra_filtering_params'));
+		return Balanced.ModelArray.newArrayLoadedFromUri(
+			this.get('results_uri'),
+			this.get('results_type')
+		);
+	}.property(
+			'fetch_results', 'useSearch', 'results_uri', 'results_type',
+			'type', 'endpoint', 'status_rollup', 'search_result.transactions',
+			'search_result.accounts', 'search_result.funding_instruments'),
 
-        if (statuses) {
-            var status_rollup = {
-                'status_rollup[in]': statuses
-            };
+	search_result: function () {
+		var self = this;
+		if (this.get('useSearch')) {
+			var search = Balanced.SearchQuery.search(
+				this.get('controllers.marketplace.uri'),
+				this.get('search_params')
+			);
 
-            $.extend(filteringParams, status_rollup);
-        } else {
-            delete filteringParams['status_rollup[in]'];
-        }
+			search.then(function (searchQuery) {
+				self.set('last_loaded_search_result', searchQuery);
+			});
 
-        this.set('extra_filtering_params', filteringParams);
-    },
+			return search;
+		}
 
-    search_uri: function () {
-        if (this.get('useSearch')) {
-            return Balanced.SearchQuery.createUri(
-                this.get('controllers.marketplace.uri'),
-                this.get('search_params')
-            );
-        }
-        return this.get('results_uri');
-    }.property('useSearch', 'results_uri', 'controllers.marketplace.uri', 'searchParams'),
+		return null;
 
-    loadMore: function (results) {
-        results.loadNextPage();
-    },
+	}.property('useSearch', 'controllers.marketplace.uri', 'search_params'),
 
-    reload: function () {
-        this.notifyPropertyChange('search_params');
-    },
+	search_uri: function () {
+		if (this.get('useSearch')) {
+			return Balanced.SearchQuery.createUri(
+				this.get('controllers.marketplace.uri'),
+				this.get('search_params')
+			);
+		}
+		return this.get('results_uri');
+	}.property('useSearch', 'results_uri', 'controllers.marketplace.uri', 'searchParams'),
 
-    results_uri: function () {
-        return Balanced.Utils.applyUriFilters(
-            this.get('results_base_uri'),
-            this.get('search_params')
-        );
-    }.property('results_base_uri', 'search_params'),
+	results_uri: function () {
+		return Balanced.Utils.applyUriFilters(
+			this.get('results_base_uri'),
+			this.get('search_params')
+		);
+	}.property('results_base_uri', 'search_params'),
 
-    search_params: function () {
-        return _.extend({
-            type: this.get('type'),
-            minDate: this.get('minDate'),
-            maxDate: this.get('maxDate'),
-            sortField: this.get('sortField'),
-            sortOrder: this.get('sortOrder'),
-            limit: this.get('limit')
-        }, this.get('extra_filtering_params'));
-    }.property('type', 'minDate', 'maxDate', 'sortField', 'sortOrder', 'limit', 'extra_filtering_params'),
+	search_params: function () {
+		return _.extend({
+			type: this.get('type'),
+			minDate: this.get('minDate'),
+			maxDate: this.get('maxDate'),
+			sortField: this.get('sortField'),
+			sortOrder: this.get('sortOrder'),
+			limit: this.get('limit')
+		}, this.get('extra_filtering_params'));
+	}.property('type', 'minDate', 'maxDate', 'sortField', 'sortOrder', 'limit', 'extra_filtering_params'),
 
-    results_type: function () {
-        switch (this.get('type')) {
-            case 'transaction':
-                return 'Balanced.Transaction';
-            case 'debit':
-                return 'Balanced.Debit';
-            case 'credit':
-                return 'Balanced.Credit';
-            case 'hold':
-                return 'Balanced.Hold';
-            case 'refund':
-                return 'Balanced.Refund';
-            case 'account':
-                return 'Balanced.Account';
-            case 'customer':
-                return 'Balanced.Customer';
-            case 'funding_instrument':
-                return 'Balanced.FundingInstrument';
-            case 'bank_account':
-                return 'Balanced.BankAccount';
-            case 'card':
-                return 'Balanced.Card';
-            case 'log':
-                return 'Balanced.Log';
-            default:
-                return null;
-        }
-    }.property('type'),
+	results_type: function () {
+		switch (this.get('type')) {
+			case 'transaction':
+				return 'Balanced.Transaction';
+			case 'debit':
+				return 'Balanced.Debit';
+			case 'credit':
+				return 'Balanced.Credit';
+			case 'hold':
+				return 'Balanced.Hold';
+			case 'refund':
+				return 'Balanced.Refund';
+			case 'account':
+				return 'Balanced.Account';
+			case 'customer':
+				return 'Balanced.Customer';
+			case 'funding_instrument':
+				return 'Balanced.FundingInstrument';
+			case 'bank_account':
+				return 'Balanced.BankAccount';
+			case 'card':
+				return 'Balanced.Card';
+			case 'log':
+				return 'Balanced.Log';
+			default:
+				return null;
+		}
+	}.property('type'),
 
-    // used for when filtering to one specific type. for example: if the user
-    // is viewing holds, type==hold category==transaction
-    category: function () {
-        var type = this.get('type');
-        if (_.contains(Balanced.SEARCH.CATEGORIES, type)) {
-            return type;
-        }
+	// used for when filtering to one specific type. for example: if the user
+	// is viewing holds, type==hold category==transaction
+	category: function () {
+		var type = this.get('type');
+		if (_.contains(Balanced.SEARCH.CATEGORIES, type)) {
+			return type;
+		}
 
-        if (_.contains(Balanced.SEARCH.TRANSACTION_TYPES, type)) {
-            return 'transaction';
-        }
+		if (_.contains(Balanced.SEARCH.TRANSACTION_TYPES, type)) {
+			return 'transaction';
+		}
 
-        if (_.contains(Balanced.SEARCH.FUNDING_INSTRUMENT_TYPES, type)) {
-            return 'funding_instrument';
-        }
+		if (_.contains(Balanced.SEARCH.FUNDING_INSTRUMENT_TYPES, type)) {
+			return 'funding_instrument';
+		}
 
-        return '';
-    }.property('type')
+		return '';
+	}.property('type')
 });
