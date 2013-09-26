31c037d4a5baf127c890fa177a712065581f75b4
zachgersh <zdgman@gmail.com>
2013-09-25 21:27:15 -0700

    wraps refactor of bank_account.js

--- test/integration/bank_account.js
+++ test/integration/bank_account.js
@@ -1,4 +1,4 @@
-var addBankAccountsRoutePath = '/marketplaces/MP5m04ORxNlNDm1bB7nkcgSY/bank_accounts/BA5r9JGZJ7YOCiULGthQYjVc'
+var addBankAccountsRoutePath = '/marketplaces/MP5m04ORxNlNDm1bB7nkcgSY/bank_accounts/BA5r9JGZJ7YOCiULGthQYjVc';
 
 module('Bank Account Page', {
 	setup: function () {
@@ -30,7 +30,7 @@ test('credit bank account', function (assert) {
   visit(addBankAccountsRoutePath)
 	.click(".main-header .buttons a.credit-button")
 	.fillIn('#credit-bank-account .modal-body input[name="dollar_amount"]', '1000')
-	.fillIn('#credit-bank-account .modal-body input[name="description"]', 'Test debit')
+	.fillIn('#credit-bank-account .modal-body input[name="description"]', 'Test credit')
 	.click('#credit-bank-account .modal-footer button[name="modal-submit"]')
   .then(function() {
     // should be one create for the debit
@@ -38,7 +38,7 @@ test('credit bank account', function (assert) {
     assert.ok(spy.calledWith(Balanced.Credit, '/v1/customers/AC5m0wzuMTw3JbKP4uIZXFpC/credits', {
       amount: 100000,
       bank_account_uri: "/v1/customers/CU1DkfCFcAemmM99fabUso2c/bank_accounts/BA5r9JGZJ7YOCiULGthQYjVc",
-      description: "Test debit"
+      description: "Test credit"
     }));
   });
 });
@@ -49,7 +49,10 @@ test('crediting only submits once despite multiple clicks', function (assert) {
   visit(addBankAccountsRoutePath)
 	.click(".main-header .buttons a.credit-button")
 	.fillIn('#credit-bank-account .modal-body input[name="dollar_amount"]', '1000')
-	.fillIn('#credit-bank-account .modal-body input[name="description"]', 'Test debit')
+	.fillIn('#credit-bank-account .modal-body input[name="description"]', 'Test credit')
+	.click('#credit-bank-account .modal-footer button[name="modal-submit"]')
+	.click('#credit-bank-account .modal-footer button[name="modal-submit"]')
+	.click('#credit-bank-account .modal-footer button[name="modal-submit"]')
 	.click('#credit-bank-account .modal-footer button[name="modal-submit"]')
   .then(function() {
     assert.ok(stub.calledOnce);
@@ -57,85 +60,79 @@ test('crediting only submits once despite multiple clicks', function (assert) {
 });
 
 test('debit bank account', function (assert) {
-	var createsBefore = Balanced.Adapter.creates.length;
+  var spy = sinon.spy(Balanced.Adapter, "create");
 
-	assert.equal($(".main-header .buttons a.debit-button").length, 1);
-
-	// click the debit button
-	$(".main-header .buttons a.debit-button").click();
-
-	$('#debit-funding-instrument .modal-body input[name="dollar_amount"]').val("1000").trigger('keyup');
-	$('#debit-funding-instrument .modal-body input[name="description"]').val("Test debit").trigger('keyup');
-
-	// click debit
-	$('#debit-funding-instrument .modal-footer button[name="modal-submit"]').click();
-
-	// should be one create for the debit
-	assert.equal(Balanced.Adapter.creates.length, createsBefore + 1);
+  visit(addBankAccountsRoutePath)
+	.click(".main-header .buttons a.debit-button")
+	.fillIn('#debit-funding-instrument .modal-body input[name="dollar_amount"]', '1000')
+	.fillIn('#debit-funding-instrument .modal-body input[name="description"]', 'Test debit')
+	.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+  .then(function() {
+    assert.ok(spy.calledOnce);
+    assert.ok(spy.calledWith(Balanced.Debit, '/v1/bank_accounts/BA5r9JGZJ7YOCiULGthQYjVc/debits', {
+      amount: 100000,
+      description: "Test debit",
+      source_uri: "/v1/customers/CU1DkfCFcAemmM99fabUso2c/bank_accounts/BA5r9JGZJ7YOCiULGthQYjVc"
+    }));
+  });
 });
 
 test('debiting only submits once despite multiple clicks', function (assert) {
 	var stub = sinon.stub(Balanced.Adapter, "create");
 
-	// click the debit button
-	$(".main-header .buttons a.debit-button").click();
-
-	$('#debit-funding-instrument .modal-body input[name="dollar_amount"]').val("1000").trigger('keyup');
-	$('#debit-funding-instrument .modal-body input[name="description"]').val("Test debit").trigger('keyup');
-
-	// click debit
-	for (var i = 0; i < 20; i++) {
-		$('#debit-funding-instrument .modal-footer button[name="modal-submit"]').click();
-	}
-
-	assert.ok(stub.calledOnce);
+  visit(addBankAccountsRoutePath)
+	.click(".main-header .buttons a.debit-button")
+	.fillIn('#debit-funding-instrument .modal-body input[name="dollar_amount"]', '1000')
+	.fillIn('#debit-funding-instrument .modal-body input[name="description"]', 'Test debit')
+	.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+	.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+	.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+	.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+  .then(function() {
+    assert.ok(stub.calledOnce);
+  });
 });
 
 test('can initiate bank account verification', function(assert) {
 	var stub = sinon.stub(Balanced.Adapter, "create");
 
-	// click the settings link
-	$('#marketplace-nav .settings a').click();
-
-	// click on the third bank account (which has no verification)
-	$(".bank-account-info .sidebar-items li").eq(2).find(".name").click();
-
-	assert.equal($('#content h1').text().trim(), 'Bank Account');
-
-	assert.equal($(".main-header .buttons a.verify-button").length, 1, 'has verify button');
-
-	// click the verify button
-	$(".main-header .buttons a.verify-button").click();
-
-	assert.equal($('#verify-bank-account').css('display'), 'block', 'verify bank account modal visible');
-
-	$('#verify-bank-account .modal-footer button[name="modal-submit"]').click();
-
-	assert.ok(stub.calledOnce);
+  visit(addBankAccountsRoutePath)
+	.click('#marketplace-nav .settings a')
+	.click(".bank-account-info .sidebar-items li:eq(2) .name")
+  .then(function() {
+    assert.equal($('#content h1').text().trim(), 'Bank Account');
+    assert.equal($(".main-header .buttons a.verify-button").length, 1, 'has verify button');
+  })
+  .then(function() {
+    click(".main-header .buttons a.verify-button");
+    assert.equal($('#verify-bank-account').css('display'), 'block', 'verify bank account modal visible');
+  })
+  .then(function() {
+    click('#verify-bank-account .modal-footer button[name="modal-submit"]');
+    assert.ok(stub.calledOnce);
+  });
 });
 
 test('can confirm bank account verification', function(assert) {
 	var stub = sinon.stub(Balanced.Adapter, "update");
 
-	// click the settings link
-	$('#marketplace-nav .settings a').click();
+  visit(addBankAccountsRoutePath)
+	.click('#marketplace-nav .settings a')
+	.click(".bank-account-info .sidebar-items li:eq(3) .name")
+  .then(function() {
+    assert.equal($('#content h1').text().trim(), 'Bank Account');
+    assert.equal($(".main-header .buttons a.confirm-verification-button").length, 1, 'has confirm button');
+  })
+  .then(function() {
+    click(".main-header .buttons a.confirm-verification-button");
 
-	// click on the fourth bank account (which has a pending verification)
-	$(".bank-account-info .sidebar-items li").eq(3).find(".name").click();
+    assert.equal($('#confirm-verification').css('display'), 'block', 'confirm verification modal visible');
 
-	assert.equal($('#content h1').text().trim(), 'Bank Account');
+    fillIn('#confirm-verification .modal-body input[name="amount_1"]', '1.00');
+    fillIn('#confirm-verification .modal-body input[name="amount_2"]', '1.00');
 
-	assert.equal($(".main-header .buttons a.confirm-verification-button").length, 1, 'has confirm button');
+    click('#confirm-verification .modal-footer button[name="modal-submit"]');
 
-	// click the confirm button
-	$(".main-header .buttons a.confirm-verification-button").click();
-
-	assert.equal($('#confirm-verification').css('display'), 'block', 'confirm verification modal visible');
-
-	$('#confirm-verification .modal-body input[name="amount_1"]').val('1.00').trigger('keyup');
-	$('#confirm-verification .modal-body input[name="amount_2"]').val('1.00').trigger('keyup');
-
-	$('#confirm-verification .modal-footer button[name="modal-submit"]').click();
-
-	assert.ok(stub.calledOnce);
+    assert.ok(stub.calledOnce);
+  });
 });
