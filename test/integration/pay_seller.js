d846daa22fd9110034fc812f100e1013f9cb376f
Dali Zheng <dali@series.ac>
2013-10-08 15:25:51 -0700

    pass pay seller tests

--- test/integration/pay_seller.js
+++ test/integration/pay_seller.js
@@ -1,53 +1,46 @@
+var marketplaceRoute;
+
 module('Pay Seller', {
 	setup: function() {
-		Testing.selectMarketplaceByName();
+		marketplaceRoute = '/marketplaces/' + Balanced.TEST.MARKETPLACE_ID;
 	},
 	teardown: function() {
-		Ember.run(function() {
-			$("#pay-seller").modal('hide');
-		});
+		$("#pay-seller").modal('hide');
 	}
 });
 
 test('can pay a seller', function(assert) {
-	var createsBefore = Balanced.Adapter.creates.length;
+	var stub = sinon.stub(Balanced.Adapter, "create");
 
-	// click the button to pay a seller
-	$('li.pay-seller a').click();
-
-	// fill out information
-	$('#pay-seller .modal-body input').eq(0).val('TEST').trigger('keyup');
-	$('#pay-seller .modal-body input').eq(1).val('123123123').trigger('keyup');
-	$('#pay-seller .modal-body input').eq(2).val('123123123').trigger('keyup');
-	$('#pay-seller .modal-body select').eq(0).val('checking').trigger('keyup');
-	$('#pay-seller .modal-body input').eq(3).val('98').trigger('keyup');
-	$('#pay-seller .modal-body input').eq(4).val('Test Transaction').trigger('keyup');
-	// click save
-	$('#pay-seller .modal-footer button')[1].click();
-
-	// should be one create
-	assert.equal(Balanced.Adapter.creates.length, createsBefore + 1);
+	visit(marketplaceRoute)
+		.click('li.pay-seller a')
+		.fillIn('#pay-seller .modal-body input:eq(0)', 'TEST')
+		.fillIn('#pay-seller .modal-body input:eq(1)', '123123123')
+		.fillIn('#pay-seller .modal-body input:eq(2)', '123123123')
+		.fillIn('#pay-seller .modal-body select:eq(0)', 'checking')
+		.fillIn('#pay-seller .modal-body input:eq(3)', '98')
+		.fillIn('#pay-seller .modal-body input:eq(4)', 'Test Transaction')
+		.click('#pay-seller .modal-footer button:eq(1)')
+		.then(function() {
+			assert.ok(stub.calledOnce);
+		});
 });
 
 test('pay a seller only submits once despite multiple button clicks', function(assert) {
-	expect(1);
-
 	var stub = sinon.stub(Balanced.Adapter, "create");
 
-	// click the button to pay a seller
-	$('li.pay-seller a').click();
-
-	// fill out information
-	$('#pay-seller .modal-body input').eq(0).val('TEST').trigger('keyup');
-	$('#pay-seller .modal-body input').eq(1).val('123123123').trigger('keyup');
-	$('#pay-seller .modal-body input').eq(2).val('123123123').trigger('keyup');
-	$('#pay-seller .modal-body select').eq(0).val('checking').trigger('keyup');
-	$('#pay-seller .modal-body input').eq(3).val('98').trigger('keyup');
-	$('#pay-seller .modal-body input').eq(4).val('Test Transaction').trigger('keyup');
-	// click save
-	for (var i = 0; i < 20; i++) {
-		$('#pay-seller .modal-footer button[name="modal-submit"]').click();
-	}
-
-	assert.ok(stub.calledOnce);
+	visit(marketplaceRoute)
+		.click('li.pay-seller a')
+		.fillIn('#pay-seller .modal-body input:eq(0)', 'TEST')
+		.fillIn('#pay-seller .modal-body input:eq(1)', '123123123')
+		.fillIn('#pay-seller .modal-body input:eq(2)', '123123123')
+		.fillIn('#pay-seller .modal-body select:eq(0)', 'checking')
+		.fillIn('#pay-seller .modal-body input:eq(3)', '98')
+		.fillIn('#pay-seller .modal-body input:eq(4)', 'Test Transaction')
+		.click('#pay-seller .modal-footer button:eq(1)')
+		.click('#pay-seller .modal-footer button:eq(1)')
+		.click('#pay-seller .modal-footer button:eq(1)')
+		.then(function() {
+			assert.ok(stub.calledOnce);
+		});
 });
