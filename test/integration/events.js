d830e3f7a13769f482eb34562a99296d676a1172
Dali Zheng <dali@series.ac>
2013-10-08 14:18:02 -0700

    pass events tests

--- test/integration/events.js
+++ test/integration/events.js
@@ -1,12 +1,20 @@
 module('Events', {
-	setup: function() {},
+	setup: function() {
+		Balanced.NET.ajax({
+			url: ENV.BALANCED.API + '/v1/events',
+			type: 'get'
+		}).done(function(res) {
+			Balanced.TEST.EVENT_ID = res.items[0].id;
+		});
+	},
 	teardown: function() {}
 });
 
 test('can visit page', function(assert) {
-	visit('/marketplaces/MP5m04ORxNlNDm1bB7nkcgSY/events/EV861bf0500b8c11e3b9fb026ba7f8ec28').then(function() {
-		assert.notEqual($('#content h1').text().indexOf('credit.created'), -1, 'Title is not correct');
-		assert.equal($(".event-data-info h3").text().trim(), 'Event data');
-		assert.equal($(".webhook-info h3").text().trim(), 'Webhooks');
-	});
+	visit('/marketplaces/' + Balanced.TEST.MARKETPLACE_ID + '/events/' + Balanced.TEST.EVENT_ID)
+		.then(function() {
+			assert.notEqual($('#content h1').text().indexOf('account.created'), -1, 'Title is correct');
+			assert.equal($(".event-data-info h3").text().trim(), 'Event data');
+			assert.equal($(".webhook-info h3").text().trim(), 'Webhooks');
+		});
 });
