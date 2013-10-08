d6d8b7de7f033668af92b0412788743d56db9ad9
Dali Zheng <dali@series.ac>
2013-10-08 16:16:28 -0700

    pass reversals tests

--- test/integration/reversals.js
+++ test/integration/reversals.js
@@ -3,7 +3,38 @@ var reversalsRoute;
 module('Reversals', {
 	setup: function() {
 		Ember.run(function() {
-			
+			Balanced.Debit.create({
+				uri: '/v1/customers/' + Balanced.TEST.CUSTOMER_ID + '/debits',
+				appears_on_statement_as: 'Pixie Dust',
+				amount: 100000,
+				description: 'Cocaine'
+			}).save();
+			Balanced.BankAccount.create({
+				name: 'Test Account',
+				account_number: '1234',
+				routing_number: '122242607',
+				type: 'checking'
+			}).save().then(function(bankAccount) {
+				Balanced.TEST.BANK_ACCOUNT_ID = bankAccount.get('id');
+				Balanced.NET.ajax({
+					url: ENV.BALANCED.API + '/v1/bank_accounts/' + Balanced.TEST.BANK_ACCOUNT_ID + '/credits',
+					type: 'post',
+					data: {
+						amount: 100000
+					}
+				}).then(function(res) {
+					Balanced.TEST.CREDIT_ID = res.id;
+					return Balanced.NET.ajax({
+						url: ENV.BALANCED.API + '/v1/marketplaces/' + Balanced.TEST.MARKETPLACE_ID +
+							'/credits/' + Balanced.TEST.CREDIT_ID + '/reversals',
+						type: 'post'
+					});
+				}).done(function(res) {
+					Balanced.TEST.REVERSAL_ID = res.id;
+					reversalsRoute = '/marketplaces/' + Balanced.TEST.MARKETPLACE_ID +
+						'/reversals/' + Balanced.TEST.REVERSAL_ID;
+				});
+			});
 		});
 	},
 	teardown: function() {}
@@ -12,7 +43,7 @@ module('Reversals', {
 test('can visit page', function(assert) {
 	visit(reversalsRoute).then(function() {
 		assert.notEqual($('#content h1').text().indexOf('Reversal'), -1, 'Title is not correct');
-		assert.equal($(".reversal .transaction-description").text().trim(), 'Successful: $25.00');
+		assert.equal($(".reversal .transaction-description").text().trim(), 'Succeeded: $1000.00');
 	});
 });
 
