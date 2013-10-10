9b047f676aa467f73648beb67658889fdbd3797e
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-10-10 12:53:57 -0700

    Stop creating API keys through the auth server

--- app/models/api_key.js
+++ app/models/api_key.js
@@ -1,11 +1,5 @@
 Balanced.APIKey = Balanced.Model.extend({
-	uri: '/v1/api_keys'
+	uri: '/api_keys'
 });
 
 Balanced.TypeMappings.addTypeMapping('api_key', 'Balanced.APIKey');
-
-Balanced.Adapter.registerHostForType(Balanced.APIKey, ENV.BALANCED.AUTH);
-
-Balanced.APIKey.reopenClass({
-	serializer: Balanced.Rev0Serializer.create()
-});
