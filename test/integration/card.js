beff2ff84e6cca816764179e7277fcc93bab2dfb
Dali Zheng <dali@series.ac>
2013-10-11 12:28:13 -0700

    pass card tests

--- test/integration/card.js
+++ test/integration/card.js
@@ -1,26 +1,19 @@
-var settingsRoutePath;
+var cardRoute;
 
 module('Card Page', {
 	setup: function() {
 		Balanced.TEST.setupMarketplace();
-		settingsRoutePath = '/marketplaces/' + Balanced.TEST.MARKETPLACE_ID + '/settings';
 		Ember.run(function() {
 			Balanced.Card.create({
-				uri: '/v1/marketplaces/' + Balanced.TEST.MARKETPLACE_ID + '/cards',
-				card_number: '4444400012123434',
+				uri: '/customers/' + Balanced.TEST.CUSTOMER_ID + '/cards',
+				number: '4444400012123434',
 				name: 'Test Card',
 				expiration_year: 2020,
 				expiration_month: 11
 			}).save().then(function(card) {
-				var uri = card.uri;
-				// ghetto workaround
-				Balanced.NET.ajax({
-					url: ENV.BALANCED.API + Balanced.TEST.marketplace.get('owner_customer.uri'),
-					type: 'put',
-					data: {
-						card_uri: uri
-					}
-				});
+				Balanced.TEST.CARD_ID = card.get('id');
+				cardRoute = '/marketplaces/' + Balanced.TEST.MARKETPLACE_ID +
+					'/cards/' + Balanced.TEST.CARD_ID;
 			});
 		});
 	},
@@ -28,8 +21,7 @@ module('Card Page', {
 });
 
 test('can view card page', function(assert) {
-	visit(settingsRoutePath)
-		.click(".card-info .sidebar-items li:eq(0) .name")
+	visit(cardRoute)
 		.then(function() {
 			assert.equal($('#content h1').text().trim(), 'Card');
 			assert.equal($(".title span").text().trim(), 'Test Card (3434)');
@@ -39,34 +31,50 @@ test('can view card page', function(assert) {
 test('debit card', function(assert) {
 	var spy = sinon.spy(Balanced.Adapter, "create");
 
-	visit(settingsRoutePath)
-		.click(".card-info .sidebar-items li:eq(0) .name")
-		.click(".main-header .buttons a.debit-button")
-		.fillIn('#debit-funding-instrument .modal-body input[name="dollar_amount"]', "1000")
-		.fillIn('#debit-funding-instrument .modal-body input[name="description"]', "Test debit")
-		.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+	visit(cardRoute)
 		.then(function() {
-			assert.ok(spy.calledOnce);
-			assert.ok(spy.calledWith(Balanced.Debit, "/v1/customers/" + Balanced.TEST.CUSTOMER_ID + "/debits", sinon.match({
-				amount: 100000,
-				description: "Test debit"
-			})));
+			var controller = Balanced.__container__.lookup('controller:cards');
+			var model = controller.get('model');
+			model.set('customer', true);
+
+			// wait for computed property to fire first
+			Ember.run.next(function() {
+				click(".main-header .buttons a.debit-button")
+				.fillIn('#debit-funding-instrument .modal-body input[name="dollar_amount"]', "1000")
+				.fillIn('#debit-funding-instrument .modal-body input[name="description"]', "Test debit")
+				.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+				.then(function() {
+					assert.ok(spy.calledOnce);
+					assert.ok(spy.calledWith(Balanced.Debit, "/cards/" + Balanced.TEST.CARD_ID + "/debits", sinon.match({
+						amount: 100000,
+						description: "Test debit"
+					})));
+				});
+			});
 		});
 });
 
 test('debiting only submits once despite multiple clicks', function(assert) {
 	var stub = sinon.stub(Balanced.Adapter, "create");
 
-	visit(settingsRoutePath)
-		.click(".card-info .sidebar-items li:eq(0) .name")
-		.click(".main-header .buttons a.debit-button")
-		.fillIn('#debit-funding-instrument .modal-body input[name="dollar_amount"]', "1000")
-		.fillIn('#debit-funding-instrument .modal-body input[name="description"]', "Test debit")
-		.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
-		.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
-		.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
-		.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+	visit(cardRoute)
 		.then(function() {
-			assert.ok(stub.calledOnce);
+			var controller = Balanced.__container__.lookup('controller:cards');
+			var model = controller.get('model');
+			model.set('customer', true);
+
+			// wait for computed property to fire first
+			Ember.run.next(function() {
+				click(".main-header .buttons a.debit-button")
+				.fillIn('#debit-funding-instrument .modal-body input[name="dollar_amount"]', "1000")
+				.fillIn('#debit-funding-instrument .modal-body input[name="description"]', "Test debit")
+				.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+				.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+				.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+				.click('#debit-funding-instrument .modal-footer button[name="modal-submit"]')
+				.then(function() {
+					assert.ok(stub.calledOnce);
+				});
+			});
 		});
 });
