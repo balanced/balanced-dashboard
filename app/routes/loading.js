aae60ad2b1841f8de7e9006cebcdf443dabd4b02
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-10-11 14:39:03 -0700

    Fix warnings

--- app/routes/loading.js
+++ app/routes/loading.js
@@ -1,14 +1,15 @@
 Balanced.LoadingRoute = Ember.Route.extend({
 	deactivate: function() {
 		var timer = this.get('timer');
-		if (timer)
-			Em.run.cancel(timer);
+		if (timer) {
+			Ember.run.cancel(timer);
+		}
 	},
 
 	renderTemplate: function(controller, model) {
 		var self = this;
 		// Only render the loading indicator after 0.25s
-		var timer = Em.run.later(this, function() {
+		var timer = Ember.run.later(this, function() {
 			self.render('loading');
 		}, 250);
 
