9b051374d078599972b3c6734689fa63b5a1fdf6
Tarun Chaudhry <tarunc92@gmail.com>
2013-10-21 17:40:23 -0700

    fixed integration tests for invoices

--- test/integration/invoices.js
+++ test/integration/invoices.js
@@ -46,7 +46,7 @@ test('invoice detail page', function(assert) {
 			assert.equal($(".refund-details-row .total").text().trim(), "-$2.45");
 			assert.equal($(".reversal-details-row .total").text().trim(), "$0.00");
 			assert.equal($(".chargeback-details-row .total").text().trim(), "$0.00");
-			assert.equal($(".subtotal-row .total").text().trim(), "$17.85");
+			assert.equal($(".subtotal-row:first .total").text().trim(), "$17.85");
 			assert.equal($(".adjustments-row .total").text().trim(), "$0.00");
 			assert.equal($(".total-balance-row .total").text().trim(), "$17.85");
 		})
