5b97ceabab6e99a352252e13a1351bea7597143f
Tarun Chaudhry <tarunc92@gmail.com>
2013-11-22 17:44:13 -0800

    formated code

--- app/views/marketplaces/search_marketplace.js
+++ app/views/marketplaces/search_marketplace.js
@@ -3,7 +3,6 @@ Balanced.SearchMarketplaceView = Balanced.View.extend({
 	tagName: 'form',
 
 	query: null,
-	results: null,
 
 	isSubmitting: false,
 
@@ -11,16 +10,14 @@ Balanced.SearchMarketplaceView = Balanced.View.extend({
 		return this.get('query') && this.get('results');
 	}.property('results', 'query'),
 
-	results: function () {
+	results: function() {
 		var query = this.get('query');
-		console.log(query);
 
 		if (!query) {
 			return this.set('isSubmitting', false);
 		}
 
 		if (this.get('isSubmitting')) {
-			console.log('isSubmitting')
 			return;
 		}
 		this.set('isSubmitting', true);
@@ -28,26 +25,10 @@ Balanced.SearchMarketplaceView = Balanced.View.extend({
 		var self = this;
 
 		var searchArray = Balanced.ModelArray.newArrayLoadedFromUri(ENV.BALANCED.AUTH + '/admin/marketplaces?q=' + encodeURIComponent(query), 'Balanced.Marketplace');
-		searchArray.on('didLoad', function () {
+		searchArray.on('didLoad', function() {
 			self.set('isSubmitting', false);
 		});
 
 		return searchArray;
-	}.property('query'),
-
-	actions: {
-		submit: function() {
-			console.log('submit', this.get('query'));
-
-			// Balanced.NET.ajax({
-			// 	url: ,
-			// 	type: 'GET'
-			// }).done(function (response, status, jqxhr) {
-			// 	console.log(response);
-			// 	self.set('results', response);
-			// }).complete(function () {
-			// 	self.set('isSubmitting', false);
-			// });
-		}
-	}
+	}.property('query')
 });
