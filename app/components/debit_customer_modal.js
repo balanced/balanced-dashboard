62791f5865dffc1247d21da7367e9f72edff9f33
Marshall Jones <marshall@balancedpayments.com>
2013-12-02 16:39:59 -0800

    credit and debit to funding instrument rather than to customer

--- app/components/debit_customer_modal.js
+++ app/components/debit_customer_modal.js
@@ -1,69 +1,68 @@
 Balanced.DebitCustomerModalComponent = Ember.Component.extend({
-	submitAction: 'submitDebitCustomer',
-	classNames: ['modal-container'],
+    submitAction: 'submitDebitCustomer',
+    classNames: ['modal-container'],
 
-	willDestroyElement: function() {
-		$('#debit-customer').modal('hide');
-	},
+    willDestroyElement: function () {
+        $('#debit-customer').modal('hide');
+    },
 
-	dollar_amount: null,
+    dollar_amount: null,
 
-	actions: {
-		open: function() {
-			var fundingInstruments = this.get('customer.debitable_funding_instruments');
-			var source_uri = (fundingInstruments && fundingInstruments.length > 0) ? fundingInstruments[0].get('uri') : null;
+    actions: {
+        open: function () {
+            var fundingInstruments = this.get('customer.debitable_funding_instruments');
+            var debitUri = (fundingInstruments && fundingInstruments.length > 0) ? fundingInstruments[0].get('debits_uri') : null;
 
-			var debit = Balanced.Debit.create({
-				uri: this.get('customer.debits_uri'),
-				source_uri: source_uri,
-				amount: null,
-				order: this.get('order.href')
-			});
+            var debit = Balanced.Debit.create({
+                uri: debitUri,
+                amount: null,
+                order: this.get('order.href')
+            });
 
-			debit.on('didCreate', function() {
-				$('#debit-customer').modal('hide');
-			});
+            debit.on('didCreate', function () {
+                $('#debit-customer').modal('hide');
+            });
 
-			this.set('dollar_amount', null);
-			this.set('model', debit);
+            this.set('dollar_amount', null);
+            this.set('model', debit);
 
-			$('#debit-customer').modal({
-				manager: this.$()
-			});
-		},
+            $('#debit-customer').modal({
+                manager: this.$()
+            });
+        },
 
-		save: function() {
-			if (this.get('model.isSaving')) {
-				return;
-			}
+        save: function () {
+            if (this.get('model.isSaving')) {
+                return;
+            }
 
-			var debit = this.get('model');
+            var debit = this.get('model');
 
-			var cents = null;
-			try {
-				cents = Balanced.Utils.dollarsToCents(this.get('dollar_amount'));
-			} catch (error) {
-				debit.set('validationErrors', {
-					'amount': error
-				});
-				return;
-			}
-			debit.set('amount', cents);
+            var cents = null;
+            try {
+                cents = Balanced.Utils.dollarsToCents(this.get('dollar_amount'));
+            } catch (error) {
+                debit.set('validationErrors', {
+                    'amount': error
+                });
+                return;
+            }
+            debit.set('amount', cents);
 
-			this.sendAction('submitAction', debit);
-		}
-	},
+            this.sendAction('submitAction', debit);
+        }
+    },
 
-	selected_funding_instrument: function() {
-		if (this.get('model.source_uri')) {
-			var self = this;
-			return this.get('customer.debitable_funding_instruments').find(function(i) {
-				return self.get('model.source_uri') === i.get('uri');
-			});
-		}
-	}.property('model.source_uri', 'customer.debitable_funding_instruments'),
+    selected_funding_instrument: function () {
+        var sourceUri = this.get('model.source_uri');
+        if (sourceUri) {
+            return this.get('customer.debitable_funding_instruments').find(function (fundingInstrument) {
+                return sourceUri === fundingInstrument.get('uri');
+            });
+        }
+    }.property('model.source_uri', 'customer.debitable_funding_instruments'),
 
-	can_debit: function() {
-		return this.get('customer.debitable_funding_instruments.length') > 0;
-	}.property('customer.debitable_funding_instruments')
+    can_debit: function () {
+        return this.get('customer.debitable_funding_instruments.length') > 0;
+    }.property('customer.debitable_funding_instruments')
 });
