7d2cc555a2cae1b0d46ee02ac98c5d828cc76534
Dali Zheng <dali@series.ac>
2013-12-12 16:42:40 -0800

    check if a debit is refundable

--- app/models/debit.js
+++ app/models/debit.js
@@ -5,6 +5,8 @@ Balanced.Debit = Balanced.Transaction.extend({
 	hold: Balanced.Model.belongsTo('hold', 'Balanced.Hold'),
 	refunds: Balanced.Model.hasMany('refunds', 'Balanced.Refund'),
 
+	refund_amount: 0,
+
 	type_name: function() {
 		return "Debit";
 	}.property(),
@@ -15,7 +17,22 @@ Balanced.Debit = Balanced.Transaction.extend({
 
 	funding_instrument_description: function() {
 		return this.get('source.description');
-	}.property('source.description')
+	}.property('source.description'),
+
+	get_refunds: function() {
+		var _this = this;
+		this.get('refunds').then(function(refunds) {
+			var accumulator = 0;
+			refunds.forEach(function(refund) {
+				accumulator += refund.amount;
+			});
+			_this.set('refund_amount', accumulator);
+		});
+	}.on('didLoad'),
+
+	can_refund: function() {
+		return this.get('amount') > this.get('refund_amount');
+	}.property('amount', 'refund_amount')
 });
 
 Balanced.TypeMappings.addTypeMapping('debit', 'Balanced.Debit');
