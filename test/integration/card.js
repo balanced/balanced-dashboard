0d80a5013fb83f5377233adfd9d2088d233bb3e4
Kyungmin Kim <kmin905@gmail.com>
2014-01-20 17:38:35 -0800

    Apply unit test patch

--- test/integration/card.js
+++ test/integration/card.js
@@ -70,22 +70,26 @@ test('debiting only submits once despite multiple clicks', function(assert) {
 		});
 });
 
-test('renders metadata correctly', function (assert) {
-  Balanced.Card.findAll().then(function (cards) {
-    Ember.run(function() {
-      var card = cards.content[0];
-      card.set('meta', {
-          'key': 'value',
-          'other-key': 'other-value'
-      });
+test('renders metadata correctly', function(assert) {
+	var metaData = {
+		'key': 'value',
+		'other-keey': 'other-vaalue'
+	};
+	Ember.run(function() {
+		Balanced.Card.findAll().then(function(cards) {
+			var card = cards.content[0];
+			card.set('meta', metaData);
 
-      card.save().then(function (debit) {
-        var cardUri = Testing.CARD_ROUTE = '/marketplaces/' + Testing.MARKETPLACE_ID + '/cards/' + card.get('id');
-        visit(cardUri).then(function () {
-					assert.equal($('.card-info').find('.control-label').last().text().trim(), 'other-key');
-					assert.equal($('.card-info').find('.inline-label').last().text().trim(), 'other-value');
-        });
-      });
-    });
-  });
+			card.save().then(function(card) {
+				var cardPageUrl = Testing.CARD_ROUTE = '/marketplaces/' + Testing.MARKETPLACE_ID + '/cards/' + card.get('id');
+				visit(cardPageUrl).then(function() {
+					var $controlGroups = $('.card-info .control-group');
+					$.each(metaData, function(key, value) {
+						assert.equal($controlGroups.find('.control-label:contains("' + key + '")').length, 1);
+						assert.equal($controlGroups.find('.inline-label:contains("' + value + '")').length, 1);
+					});
+				});
+			});
+		});
+	});
 });
