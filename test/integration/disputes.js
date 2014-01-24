7abdf04193b3b75ffd2ed1ae2350dcf4e3937b94
Tarun Chaudhry <tarunc92@gmail.com>
2014-01-24 12:15:33 -0800

    fixing dispute tests

--- test/integration/disputes.js
+++ test/integration/disputes.js
@@ -8,7 +8,7 @@ module('Disputes', {
 
 test('can visit page', function(assert) {
 	visit(Testing.DISPUTE_ROUTE).then(function() {
-		assert.notEqual($('#content h1').text().indexOf('Debit'), -1, 'Title is not correct');
+		assert.notEqual($('#content h1').text().indexOf('Dispute'), -1, 'Title is not correct');
 		assert.equal($(".debit .transaction-description").text().trim(), 'Succeeded: $100.00');
 	});
 });
