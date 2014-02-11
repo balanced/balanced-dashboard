9598e90e31a6d5eb3c1f3b30c1bcb33c0f91e0b6
Tarun Chaudhry <tarunc92@gmail.com>
2014-02-11 13:31:37 -0800

    adding test

--- test/integration/reset_password.js
+++ test/integration/reset_password.js
@@ -22,13 +22,31 @@ test('setting a weak password should error', function(assert) {
 		});
 });
 
-test('reset password form submits', function(assert) {
+test('reset password form submits on button click', function(assert) {
 	var spy = sinon.spy(Balanced.Adapter, "update");
 
 	visit('/password/abcdefghijklmnopq')
 		.fillIn("form#reset-password-form input[name=password]", 'abcdef5')
 		.fillIn("form#reset-password-form input[name=password_confirm]", 'abcdef5')
 		.click("form#reset-password-form button").then(function() {
+			assert.equal($("div#content div.alert-black").length, 1, 'The black confirmation box is visible');
+
+			assert.ok(spy.calledOnce);
+			assert.ok(spy.calledWith(Balanced.ResetPassword, '/password/abcdefghijklmnopq', sinon.match({
+				password: 'abcdef5',
+				password_confirm: 'abcdef5',
+				token: 'abcdefghijklmnopq'
+			})));
+		});
+});
+
+test('reset password form submits on form submit', function(assert) {
+	var spy = sinon.spy(Balanced.Adapter, "update");
+
+	visit('/password/abcdefghijklmnopq')
+		.fillIn("form#reset-password-form input[name=password]", 'abcdef5')
+		.fillIn("form#reset-password-form input[name=password_confirm]", 'abcdef5')
+		.submitForm("form#reset-password-form").then(function() {
 			assert.equal($("div#content div.alert-black").length, 1, 'The black confirmation box is visible');
 
 			assert.ok(spy.calledOnce);
