db6621dee000a90fc6771714b12cc671c1c2600f
Tarun Chaudhry <tarunc92@gmail.com>
2014-02-11 12:49:42 -0800

    disputes

--- test/integration/disputes.js
+++ test/integration/disputes.js
@@ -26,7 +26,7 @@ test('exist on the activity page', function(assert) {
 			// Manual check the disputes uri is correct
 			var activityController = Balanced.__container__.lookup('controller:activity');
 			assert.equal(activityController.get('results_base_uri'), '/disputes', 'Disputes URI is correct');
-			assert.ok(activityController.get('results_uri').indexOf('sort=created_at') > 0, 'Disputes Sort is correct');
+			assert.ok(activityController.get('results_uri').indexOf('sort=initiated_at') > 0, 'Disputes Sort is correct');
 		})
 		.checkElements(activityDisputesPage, assert)
 		.click('#activity .results table.disputes tfoot td.load-more-results a')
