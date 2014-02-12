899f1d5644e890937207aeead2380fa621a8e119
Tarun Chaudhry <tarunc92@gmail.com>
2014-02-11 18:29:17 -0800

    redirecting back from account security

--- app/routes/account_security.js
+++ app/routes/account_security.js
@@ -1,3 +1,5 @@
+var NOT_ALLOWED_ROUTES = ['shapeshifter'];
+
 Balanced.AccountSecurityRoute = Balanced.AuthRoute.extend({
 	pageTitle: 'Account Security',
 
@@ -14,5 +16,45 @@ Balanced.AccountSecurityRoute = Balanced.AuthRoute.extend({
 		return {
 			emptyModel: emptyModel
 		};
+	},
+
+	beforeModel: function(transition) {
+		var router = Balanced.__container__.lookup('router:main').get('router');
+		var currentHandlerInfos = router.currentHandlerInfos;
+		if (!currentHandlerInfos || !currentHandlerInfos.length) {
+			this.previousHandler = null;
+			return;
+		}
+
+		var previousHandler = currentHandlerInfos[currentHandlerInfos.length - 1];
+		if (NOT_ALLOWED_ROUTES.indexOf(previousHandler.name) >= 0) {
+			this.previousHandler = null;
+			return;
+		}
+
+		this.previousHandler = previousHandler;
+	},
+
+	goBack: function() {
+		var name = this.previousHandler && this.previousHandler.name;
+
+		if (!name) {
+			name = 'marketplaces';
+			return;
+		}
+
+		if (this.previousHandler.context) {
+			this.transitionTo(name, this.previousHandler.context);
+		} else {
+			this.transitionTo(name);
+		}
+
+		this.previousHandler = null;
+	},
+
+	actions: {
+		goBack: function() {
+			return this.goBack();
+		}
 	}
 });
