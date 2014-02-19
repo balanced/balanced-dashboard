ff682286afb5a52a83b06572c6a762da44b1f96e
Tarun Chaudhry <tarunc92@gmail.com>
2014-02-18 17:03:24 -0800

    fixing dispute tests

--- test/integration/disputes.js
+++ test/integration/disputes.js
@@ -14,7 +14,7 @@ test('exist on the activity page', function(assert) {
 		'table.disputes tbody tr:eq(0) td.type': 'Pending',
 		'table.disputes tbody tr:eq(0) td.account': 1,
 		'table.disputes tbody tr:eq(0) td.funding-instrument': 1,
-		'table.disputes tbody tr:eq(0) td.amount': '-$100.00'
+		'table.disputes tbody tr:eq(0) td.amount': '$100.00'
 	};
 
 	visit(Testing.MARKETPLACE_ROUTE + '/activity/disputes')
@@ -29,7 +29,7 @@ test('can visit page', function(assert) {
 		'#content h1': 'Dispute',
 		'#dispute > .main-header .title': 1, // 'Brand New Electric Guitar Rosewood Fingerboard Sunset Red',
 		'#dispute .customer-info .main-header .title': 1, // 'William Henry Cavendish III (whc@example.org)',
-		'#dispute .transaction-details .dispute .transaction-type-subheader .title': 'Pending: -$100.00',
+		'#dispute .transaction-details .dispute .transaction-type-subheader .title': 'Pending: $100.00',
 		'#dispute .transaction-details .debit .transaction-type-subheader .title': 1 // 'Succeeded: $13.30'
 	};
 
