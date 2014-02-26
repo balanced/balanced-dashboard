4c3ad399db6014d886467abb7ea7a31e9d884757
Patrick Conway <conway.pd@gmail.com>
2014-02-26 15:59:53 -0800

    prep work for receiving additional key data from auth

--- app/controllers/marketplace_settings.js
+++ app/controllers/marketplace_settings.js
@@ -1,30 +1,45 @@
 Balanced.MarketplaceSettingsController = Balanced.ObjectController.extend(Ember.Evented, {
 	needs: ["marketplace"],
-	keys: [],
 
 	can_edit: Ember.computed.alias('production'),
+
+	keys: [],
 
 	userMarketplace: function() {
 		var user = this.get('user');
 		var currentUserMarketplace = user.user_marketplace_for_id(this.get('id'));
 		return currentUserMarketplace;
-	}.property(),
+	}.property('id'),
 
-	init: function() {
+	keyUpdater: function() {
+		window.ms = this;
 		var self = this;
+		var knownKeys = this.get('userMarketplace.keys');
+		var secrets = {};
+		if (knownKeys) {
+			knownKeys.forEach(function(key) {
+				secrets[key.uri] = key.secret;
+			});
+		}
 		Balanced.APIKey.findAll()
 			.then(function(result) {
 				var keys = result.content;
 				var date1, date2;
+				var secret;
 				keys.sort(function(k1, k2) {
 					date1 = k1.get('created_at');
 					date2 = k2.get('created_at');
 					return date1 < date2 ? 1 : -1;
 				});
+				keys.forEach(function(key) {
+					secret = secrets[key.get('uri')];
+					if (secret) {
+						key.set('secret', secret);
+					}
+				});
 				self.set('keys', keys);
 			});
-		this._super();
-	},
+	}.observes('id'),
 
 	actions: {
 		promptToDeleteCallback: function(callback) {
