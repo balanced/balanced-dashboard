16e4e3688310fde009d594539851b1065f206882
Kyungmin Kim <kmin905@gmail.com>
2014-02-28 12:33:29 -0800

    Resolve merge conflict

--- Gruntfile.js
+++ Gruntfile.js
@@ -633,15 +633,6 @@ module.exports = function(grunt) {
 					livereload: true,
 				}
 			},
-			fonts: {
-				files: [
-					'static/fonts/**/*'
-				],
-				tasks: ['_buildFonts'],
-				options: {
-					livereload: true,
-				}
-			},
 			html: {
 				files: [
 					'app/index.html.hbs'
