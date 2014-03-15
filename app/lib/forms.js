7056aaa8bd3f9739433a01f35552c5637c42e16d
Tarun Chaudhry <tarunc92@gmail.com>
2014-03-14 17:18:22 -0700

    fix textfields

--- app/lib/forms.js
+++ app/lib/forms.js
@@ -1,3 +1,12 @@
+// Hack to make it injest whatever value is there
+Ember.TextField.reopen({
+	didInsertElement: function() {
+		// Call hidden function _elementValueDidChange
+		// when value of textfield changes.
+		_.defer(_.bind(this._elementValueDidChange, this));
+	}
+});
+
 Balanced.Forms = {
 	TextField: Ember.TextField.extend({
 		attributeBindings: ['autocomplete', 'placeholder', 'autofocus']
