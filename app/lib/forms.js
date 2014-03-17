61b7b1448e7e19acb018b3b1773c38043a04179b
Tarun Chaudhry <tarunc92@gmail.com>
2014-03-17 12:08:10 -0700

    comments

--- app/lib/forms.js
+++ app/lib/forms.js
@@ -1,15 +1,18 @@
 // Hack to make it injest whatever value is there
 Ember.TextField.reopen({
 	didInsertElement: function() {
-		// Call hidden function _elementValueDidChange
-		// when value of textfield changes.
 		var self = this;
+
+		// Let it be defered for the Browser/password managers
+		// to put in the value into the textfield
 		_.defer(function() {
 			// Check if the textfield is valid in DOM
 			if (!self.isVisible || self.isDestroyed || self.isDestroying) {
 				return;
 			}
 
+			// Call hidden function _elementValueDidChange
+			// when value of textfield changes.
 			self._elementValueDidChange();
 		});
 	}
