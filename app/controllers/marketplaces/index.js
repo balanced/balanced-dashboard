97fed0b419c9e68eaf2dcf4586290d9214f5a1c1
Tarun Chaudhry <tarunc92@gmail.com>
2014-03-24 12:01:33 -0700

    goToMarketplace

--- app/controllers/marketplaces/index.js
+++ app/controllers/marketplaces/index.js
@@ -3,6 +3,9 @@ Balanced.MarketplacesIndexController = Balanced.ArrayController.extend(Ember.Eve
 	actions: {
 		promptToDeleteMarketplace: function(marketplace) {
 			this.trigger('openDeleteMarketplaceModal', marketplace);
+		},
+		goToMarketplace: function(marketplace) {
+			this.transitionTo('marketplace', marketplace);
 		}
 	}
 });
