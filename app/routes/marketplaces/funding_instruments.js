47db01fbe36b60ab6302d8e6710c1d79404a59c1
Kyungmin Kim <kmin905@gmail.com>
2014-04-08 11:38:20 -0700

    Add funding instruments pages

--- app/routes/marketplaces/funding_instruments.js
+++ app/routes/marketplaces/funding_instruments.js
@@ -6,22 +6,3 @@ Balanced.FundingInstrumentsIndexRoute = Balanced.AuthRoute.extend({
 		controller.send('reload');
 	}
 });
-
-Balanced.CustomersCustomerRoute = Balanced.AuthRoute.extend({
-	pageTitle: function(route, setTitle) {
-		var customer = route.controller.content;
-		return Balanced.Utils.maybeDeferredLoading(customer, setTitle, function() {
-			return 'Customer: loading ...';
-		}, function() {
-			return 'Customer: %@'.fmt(customer.get('displayName'));
-		});
-	},
-
-	model: function(params) {
-		var marketplace = this.modelFor('marketplace');
-		return marketplace.then(function(marketplace) {
-			var customerUri = Balanced.Utils.combineUri(marketplace.get('customers_uri'), params.customer_id);
-			return Balanced.Customer.find(customerUri);
-		});
-	}
-});
