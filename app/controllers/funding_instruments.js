d40e3f8f6548a0a07a599868468135772cccce5f
Kyungmin Kim <kmin905@gmail.com>
2014-04-09 13:45:03 -0700

    Fix funding instrument uri

--- app/controllers/funding_instruments.js
+++ app/controllers/funding_instruments.js
@@ -1,5 +1,5 @@
 Balanced.FundingInstrumentsIndexController = Balanced.ObjectController.extend(Ember.Evented, Balanced.ResultsTable, {
-	needs: ['marketplace'],
+	needs: ['marketplace', 'activity'],
 
 	sortField: 'created_at',
 	sortOrder: 'desc',
@@ -7,7 +7,11 @@ Balanced.FundingInstrumentsIndexController = Balanced.ObjectController.extend(Em
 	baseClassSelector: "#funding-instruments",
 	noDownloadsUri: true,
 
-	results_base_uri: Ember.computed.alias('controllers.marketplace.transactions_uri')
+	type: 'funding_instrument',
+
+	results_base_uri: function() {
+		return Balanced.FundingInstrument.create().get('uri');
+	}.property(),
 });
 
 Balanced.FundingInstrumentsController = Balanced.ObjectController.extend(Ember.Evented, Balanced.ResultsTable, {});
--- app/models/funding_instrument.js
+++ app/models/funding_instrument.js
@@ -15,12 +15,5 @@ Balanced.FundingInstrument = Balanced.Model.extend(
 			return '%@: %@'.fmt(this.get('type_name'), this.get('description'));
 		}.property('description'),
 
-		// TODO - fix the API to return the transactions_uri, then get rid of this hack
-		transactions_uri: function() {
-			var id = this.get('id');
-			if (!id) {
-				return id;
-			}
-			return '/%@/%@/transactions'.fmt(this.get('route_name'), this.get('id'));
-		}.property('id', 'route_name')
+		uri: '/search'
 	});
