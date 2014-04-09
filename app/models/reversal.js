7f4571db2e5c92eb42d78392d0e8e3171e3ef9a8
Kyungmin Kim <kmin905@gmail.com>
2014-04-09 10:34:48 -0700

    Fix typo

--- app/models/reversal.js
+++ app/models/reversal.js
@@ -13,8 +13,8 @@ Balanced.Reversal = Balanced.Transaction.extend({
 
 	last_four: Ember.computed.alias('credit.last_four'),
 	funding_instrument_name: Ember.computed.alias('credit.funding_instrument_name'),
-	funding_instrument_description: Ember.computed.readOnly('credit.funding_instrument_description');
-	customer: Ember.computed.readOnly('credit.customer');
+	funding_instrument_description: Ember.computed.readOnly('credit.funding_instrument_description'),
+	customer: Ember.computed.readOnly('credit.customer')
 });
 
 Balanced.TypeMappings.addTypeMapping('reversal', 'Balanced.Reversal');
