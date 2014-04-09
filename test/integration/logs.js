cee54a4c69fb7aa3611f7c0f74d3d95f32fc4efd
Victor Lin <bornstub@gmail.com>
2014-04-09 15:27:34 -0700

    Add test for datetime range picker in log page

--- test/integration/logs.js
+++ test/integration/logs.js
@@ -70,12 +70,54 @@ test('filter logs by endpoint bank accounts', function(assert) {
 		.then(function() {
 			assert.equal($('table.logs tbody tr').length, 2, 'has 2 logs');
 		})
-		.click('.results li.filter-endpoints ul a.bank_accounts')
+		.click('.results div.filter-endpoints ul a.bank_accounts')
 		.then(function() {
 			assert.ok(spy.calledWith(Balanced.Log, '/logs?limit=2&method%5Bin%5D=post%2Cput%2Cdelete&offset=0&q=&sort=created_at%2Cdesc'));
 			assert.equal($('table.logs tbody tr').length, 1, 'has 1 log');
 		});
 });
+
+test('filter logs by datetime range', function(assert) {
+	var spy = sinon.spy(Balanced.Adapter, 'get');
+
+	visit(Testing.LOGS_ROUTE)
+		.click('#marketplace-nav i.icon-logs')
+		.waitFor(function() {
+			return $('table.logs tfoot td').length >= 1;
+		}, 'has "load more"')
+		.then(function() {
+			assert.equal($('table.logs tbody tr').length, 2, 'has 2 logs');
+		})
+		.click('.results .timing a.dropdown-toggle')
+		.then(function() {
+			$('.results .timing input[name="after"]').val('08/01/2013').trigger('keyup');
+		})
+		.click('.results .timing td.active.day')
+		.then(function() {
+			$('.results .timing input[name="before"]').val('08/01/2013').trigger('keyup');
+		})
+		.click('.results .timing td.active.day')
+		.click('.results button.go')
+		.then(function() {
+			// Notice: month 7 is Aug here for JS Date, ugly javascript...
+			// As the date time is local, we need to convert it to ISO from in UTC timezone
+			var begin = new Date(2013, 7, 1);
+			var begin_iso = encodeURIComponent(begin.toISOString());
+			var end = new Date(2013, 7, 2);
+			var end_iso = encodeURIComponent(end.toISOString());
+
+			var expected_uri = '/logs?' +
+				'created_at%5B%3C%5D=' + end_iso + '&' +
+				'created_at%5B%3E%5D=' + begin_iso + '&' +
+				'limit=2&method%5Bin%5D=post%2Cput%2Cdelete&offset=0&q=&sort=created_at%2Cdesc';
+
+			var request = spy.getCall(spy.callCount - 1);
+			assert.ok(spy.callCount, 2);
+			assert.equal(request.args[0], Balanced.Log);
+			assert.equal(request.args[1], expected_uri);
+		});
+});
+
 
 test('filter logs by request failed only', function(assert) {
 	var spy = sinon.spy(Balanced.Adapter, 'get');
