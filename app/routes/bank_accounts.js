12a302cd6c4977af7e53d48103dcd5791877443e
Tarun Chaudhry <tarunc92@gmail.com>
2014-04-09 13:56:06 -0700

    adding back bank accounts index route

--- app/routes/bank_accounts.js
+++ app/routes/bank_accounts.js
@@ -1,3 +1,9 @@
+Balanced.BankAccountsIndexRoute = Balanced.AuthRoute.extend({
+	redirect: function() {
+		this.transitionTo('activity.funding_instruments', this.modelFor('marketplace'));
+	}
+});
+
 Balanced.BankAccountsRoute = Balanced.ModelControllerRoute.extend({
 	title: 'Bank Account',
 	modelObject: Balanced.BankAccount,
