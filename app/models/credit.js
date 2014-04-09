50cc19617e458c590dcef83527fdc89f3566a53d
Kyungmin Kim <kmin905@gmail.com>
2014-04-09 11:00:44 -0700

    Update old reference to formatted_bank_name

--- app/models/credit.js
+++ app/models/credit.js
@@ -11,7 +11,7 @@ Balanced.Credit = Balanced.Transaction.extend({
 
 	funding_instrument_description: Ember.computed.alias('bank_account.description'),
 	last_four: Ember.computed.alias('bank_account.last_four'),
-	funding_instrument_name: Ember.computed.alias('bank_account.bank_account_name'),
+	funding_instrument_name: Ember.computed.alias('bank_account.formatted_bank_name'),
 	max_reversal_amount_dollars: Balanced.computed.transform('reversal_amount', Balanced.Utils.centsToDollars),
 
 
