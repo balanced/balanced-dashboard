27a02ad75cdde94edc00d21efeaa90d68d574845
Tarun Chaudhry <tarunc92@gmail.com>
2014-04-10 12:03:43 -0700

    change password

--- app/views/change_password.js
+++ app/views/change_password.js
@@ -3,6 +3,7 @@ Balanced.ChangePasswordModalView = Balanced.ChangeEmailModalView.extend({
 	controllerEventName: 'openChangePasswordModal',
 	fieldName: 'password',
 	defaultError: 'Oops, we failed to change your password. Please try again.',
+	modelKeys: ['password', 'existing_password', 'confirm_password'],
 
 	open: function() {
 		var user = Ember.copy(Balanced.Auth.get('user'), true);
