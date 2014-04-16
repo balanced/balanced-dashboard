b3eafe9300ff6cc2a195f27fe02d6217e306c12f
Tarun Chaudhry <tarunc92@gmail.com>
2014-04-16 13:30:59 -0700

    payouts

--- app/routes/import_payouts.js
+++ app/routes/import_payouts.js
@@ -1,5 +1,11 @@
 Balanced.MarketplaceImportPayoutsRoute = Balanced.AuthRoute.extend({
 	pageTitle: 'Import Payouts',
+
+	setupController: function(controller, model) {
+		controller.refresh();
+		this._super(controller, model);
+	},
+
 	renderTemplate: function() {
 		this.render('import_payouts/index');
 	}
