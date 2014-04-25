423b1b92d68737fac131565869a6b31f20fdd741
Tarun Chaudhry <tarunc92@gmail.com>
2014-04-25 12:05:25 -0700

    fix pretty print

--- app/views/pretty_print.js
+++ app/views/pretty_print.js
@@ -3,6 +3,15 @@ Balanced.PrettyPrintView = Balanced.View.extend({
 	content: null,
 
 	didInsertElement: function() {
+		console.log('didInsertElement');
+		Ember.run.scheduleOnce('afterRender', this, this.format);
+
+		this._super();
+	},
+
+	format: function() {
+		this.$('.prettyprinted').removeClass('prettyprinted');
+		this.$('.prettyprint').text(Balanced.Utils.prettyPrint(this.get('content')));
 		prettyPrint();
-	}
+	}.observes('content')
 });
