15929f47ed6ba58306bbbc5491ccc9ac6cb30476
cohitre <carlosrr@gmail.com>
2014-05-09 14:17:14 -0700

    adding two different kinds of reporters

--- karma.conf.js
+++ karma.conf.js
@@ -73,7 +73,7 @@ module.exports = function(config) {
 
 		// test results reporter to use
 		// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-		reporters: ['story', 'coverage'],
+		reporters: ['dots', 'story', 'coverage'],
 		reportSlowerThan: 20000,
 
 		coverageReporter: {
