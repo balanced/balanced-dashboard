cb3ae6c6d22d662a4fd505c137b9addb54e69412
Kyungmin Kim <kmin905@gmail.com>
2014-05-14 10:35:48 -0700

    Fix indentation

--- app/routes/marketplaces/transactions.js
+++ app/routes/marketplaces/transactions.js
@@ -1,27 +1,27 @@
 Balanced.MarketplaceTransactionsRoute = Balanced.AuthRoute.extend({
-  pageTitle: 'Transactions',
-  defaultType: 'transaction',
-  
-  setupController: function(controller, model) {
-    this._super(controller, model);
+	pageTitle: 'Transactions',
+	defaultType: 'transaction',
 
-    var activityController = controller;
-    activityController.refresh();
+	setupController: function(controller, model) {
+		this._super(controller, model);
 
-    var defaultSort = this.get('defaultSort');
-    if (defaultSort && defaultSort !== activityController.get('sortField')) {
-      activityController.set('sortField', defaultSort);
-    }
+		var activityController = controller;
+		activityController.refresh();
 
-    var defaultType = this.get('defaultType');
-    if (!defaultType) {
-      return;
-    }
+		var defaultSort = this.get('defaultSort');
+		if (defaultSort && defaultSort !== activityController.get('sortField')) {
+			activityController.set('sortField', defaultSort);
+		}
 
-    if (activityController.get('category') !== defaultType) {
-      activityController.set('type', defaultType);
-    } else {
-      activityController.send('reload');
-    }
-  }
+		var defaultType = this.get('defaultType');
+		if (!defaultType) {
+			return;
+		}
+
+		if (activityController.get('category') !== defaultType) {
+			activityController.set('type', defaultType);
+		} else {
+			activityController.send('reload');
+		}
+	}
 });
