18959ef52ce8ab512a76d4d650284acec9138c41
cohitre <carlosrr@gmail.com>
2014-05-22 11:00:33 -0700

    Adding csv upload tracking

--- app/controllers/import_payouts.js
+++ app/controllers/import_payouts.js
@@ -8,16 +8,13 @@ Balanced.MarketplaceImportPayoutsController = Balanced.Controller.extend(Ember.E
 
 	refresh: function(text) {
 		this.set("csvText", text);
-	},
 
-	validateCreditCreators: function() {
-		var creditCreators = this.get("creditCreators");
-		if (creditCreators.get("isLoaded")) {
-			creditCreators.forEach(function(item) {
-				item.validate();
-			});
+		var collection = this.get("creditCreators");
+
+		if (collection.get("length") > 0) {
+			this.trackCollectionEvent("Csv file uploaded");
 		}
-	}.observes("creditCreators.isLoaded"),
+	},
 
 	creditCreators: function() {
 		var text = this.get("csvText");
@@ -27,6 +24,12 @@ Balanced.MarketplaceImportPayoutsController = Balanced.Controller.extend(Ember.E
 			return Balanced.CreditCreatorsCollection.fromCsvText(Balanced.currentMarketplace, text);
 		} catch (e) {
 			this.set("errorMessage", "There was an error reading your CSV file");
+
+			Balanced.Analytics.trackEvent("Csv file parsing error", {
+				text: text.slice(0, 200),
+				error: e
+			});
+
 			return Balanced.CreditCreatorsCollection.create({
 				content: []
 			});
@@ -36,12 +39,16 @@ Balanced.MarketplaceImportPayoutsController = Balanced.Controller.extend(Ember.E
 	save: function(callback) {
 		var self = this;
 		var collection = self.get('creditCreators');
+
+		this.trackCollectionEvent("Csv import started");
 		collection.save(function() {
+			self.trackCollectionEvent("Csv import finished")
 			if (callback) {
 				callback();
 			}
 			var count = collection.filterBy('isSaved').get('length');
 			self.transitionToRoute('activity');
+
 			self.refresh('');
 			self.send('alert', {
 				message: '%@ payouts were successfully submitted. Payouts might take a couple seconds to appear in the transactions list.'.fmt(count),
@@ -51,9 +58,21 @@ Balanced.MarketplaceImportPayoutsController = Balanced.Controller.extend(Ember.E
 		});
 	},
 
+	trackCollectionEvent: function(message, extra) {
+		var collection = this.get("creditCreators");
+		var attributes = {
+			isExistingCustomers: collection.get("isExistingCustomers"),
+			collectionLength: collection.get("length"),
+			validLength: collection.get("valid.length"),
+			invalidLength: collection.get("invalid.length"),
+		};
+		_.extend(attributes, extra);
+		Balanced.Analytics.trackEvent(message, attributes);
+	},
+
 	actions: {
 		clearAll: function() {
-			var self = this;
+			this.trackCollectionEvent("Csv invalid rows cleared");
 
 			var collection = this.get("creditCreators");
 			collection.get("invalid").forEach(function(creator, index) {
@@ -62,10 +81,15 @@ Balanced.MarketplaceImportPayoutsController = Balanced.Controller.extend(Ember.E
 		},
 
 		removeCreditCreator: function(creator) {
+			this.trackCollectionEvent("Csv invalid rows cleared", {
+				row: creator.getErrorObject()
+			});
+
 			this.get('creditCreators').removeObject(creator);
 		},
 
 		openAddFundsModal: function() {
+			this.trackCollectionEvent("Csv add funds modal opened");
 			this.trigger('openAddFundsModal');
 		}
 	}
