68ffbdedd576973859d3f66112374a3fda3deabb
cohitre <carlosrr@gmail.com>
2014-05-24 10:57:41 -0700

    Cleaning up how we're generating sidebar links

---
+++ app/views/sidebar/marketplace_sidebar_view.js
@@ -0,0 +1,30 @@
+var SIDEBAR_ITEMS = [
+	{
+		linkText: "Activity",
+		linkIcon: "icon-activity",
+		routeName: "activity.transactions",
+	},
+	{
+		linkText: "Logs",
+		linkIcon: "icon-logs",
+		routeName: "logs.index",
+	},
+	{
+		routeName: 'invoices.index',
+		linkIcon: "icon-invoices",
+		linkText: "Invoices",
+	},
+	{
+		routeName: "marketplace.settings",
+		linkIcon: 'icon-settings',
+		linkText: 'Settings'
+	}
+];
+
+Balanced.MarketplaceSidebarView = Ember.View.extend({
+	items: function() {
+		return SIDEBAR_ITEMS.map(function(itemHash) {
+			return Balanced.BasicLinkSidebarItemView.create(itemHash);
+		});
+	}.property()
+});
---
+++ app/views/sidebar/sidebar_item_view.js
@@ -0,0 +1,24 @@
+Balanced.SidebarItemView = Ember.View.extend({
+	tagName: "li",
+
+	getRoute: function(routeName, model) {
+		var router = Balanced.Router.router;
+		return router.generate.apply(router, arguments);
+	},
+	
+});
+
+Balanced.BasicLinkSidebarItemView = Balanced.SidebarItemView.extend({
+	templateName: "sidebar/basic_link_sidebar_item",
+	linkText: "Marketplaces",
+	linkIcon: "icon-activity",
+
+	href: function() {
+		return "#" + this.getRoute(this.get("routeName"));
+	}.property("routeName"),
+
+	isCurrent: function() {
+		var currentRouteName = Balanced.Router.router.currentHandlerInfos.get("lastObject.handler.routeName");
+		return this.get("routeName") === currentRouteName;
+	}.property("href")
+});
