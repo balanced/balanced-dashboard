adceb417cdd399b2a40a60f9e24eb2e22864214e
cohitre <carlosrr@gmail.com>
2014-05-26 10:32:28 -0700

    Converting transactions table to using cell views

--- app/views/tables/table_cell_base_view.js
+++ app/views/tables/table_cell_base_view.js
@@ -1,7 +1,16 @@
 Balanced.TableCellBaseView = Ember.View.extend({
-	tagName: "td"
+	tagName: "td",
 });
 
 Balanced.LinkedDateCellView = Balanced.TableCellBaseView.extend({
-	templateName: "tables/cells/linked_date_cell"
+	templateName: "tables/cells/linked_date_cell",
+});
+
+Balanced.LinkedTextCellView = Balanced.TableCellBaseView.extend({
+	templateName: "tables/cells/linked_text_cell",
+	classNames: ["label4a"]
+});
+
+Balanced.LinkedTwoLinesCellView = Balanced.TableCellBaseView.extend({
+	templateName: "tables/cells/linked_two_lines_cell"
 });
