4e3ea9e5f8c9f0025816c4f7ddc1b4216aabcb49
Kyungmin Kim <kmin905@gmail.com>
2014-05-28 17:58:55 -0700

    Add dispute status filter

--- app/models/dispute.js
+++ app/models/dispute.js
@@ -18,13 +18,8 @@ Balanced.Dispute = Balanced.Model.extend(Balanced.MetaArrayMixin, {
 		}
 	}.property('amount'),
 
-	customer_name_summary: function() {
-		if (this.get('transaction.customer')) {
-			return this.get('transaction.customer.display_me_with_email');
-		} else {
-			return 'None';
-		}
-	}.property('transaction.customer', 'transaction'),
+	customer_display_me: Ember.computed.alias('transaction.customer.display_me'),
+	customer_email: Ember.computed.alias('transaction.customer.email'),
 
 	last_four: Ember.computed.alias('transaction.last_four'),
 	bank_name: Ember.computed.alias('transaction.bank_name'),
