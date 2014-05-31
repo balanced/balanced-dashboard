6a08ff3a2ad170dc51a5ee2d08bb7772423e7e83
cohitre <carlosrr@gmail.com>
2014-05-30 17:15:58 -0700

    Better tests for notifications.

--- app/views/notifications_view.js
+++ app/views/notifications_view.js
@@ -1,12 +1,3 @@
-var isAnyBankAccount = function(propertyName, callback) {
-	return function() {
-		var bankAccounts = this.get("marketplaceBankAccounts");
-		return bankAccounts ?
-			bankAccounts.any(callback) :
-			false;
-	}.property(propertyName);
-};
-
 Balanced.NotificationsView = Balanced.View.extend({
 	templateName: "notifications/messages",
 
@@ -14,30 +5,55 @@ Balanced.NotificationsView = Balanced.View.extend({
 	marketplace: Ember.computed.readOnly("applicationController.auth.currentMarketplace"),
 
 	marketplaceBankAccounts: function() {
-		var bankAccounts = this.get("marketplaceOwnerCustomer.bank_accounts");
-		return bankAccounts && bankAccounts.get("isLoaded") ?
-			bankAccounts : undefined;
-	}.property("marketplaceOwnerCustomer.bank_accounts.isLoaded"),
+		return this.get("isMarketplaceBankAccountsLoaded") ?
+			this.get("marketplaceOwnerCustomer.bank_accounts") :
+			undefined;
+	}.property("isMarketplaceBankAccountsLoaded"),
+
+	isMarketplaceProduction: Ember.computed.readOnly("marketplace.production"),
+	isMarketplaceMissingDebitableBankAccount: Ember.computed.not("isMarketplaceHasDebitableBankAccount"),
+	isMarketplaceBankAccountsLoaded: Ember.computed.readOnly("marketplaceOwnerCustomer.bank_accounts.isLoaded"),
+	isMarketplaceBankAccountsEmpty: Ember.computed.equal('marketplaceBankAccounts.length', 0),
+	isMarketplaceBankAccountsFull: Ember.computed.gt('marketplaceBankAccounts.length', 0),
+
+	isMarketplaceHasDebitableBankAccount: function() {
+		var bankAccounts = this.get("marketplaceBankAccounts");
+		return bankAccounts ?
+			bankAccounts.filterBy("can_debit").length > 0 :
+			false;
+	}.property("marketplaceBankAccounts.@each.can_debit"),
 
 	hasGuestNotification: Ember.computed.readOnly('applicationController.auth.isGuest'),
 
-	hasUnverifiedBankAccount: isAnyBankAccount("marketplaceBankAccounts.@each.bank_account_verification_uri", function(bankAccount) {
-		var uri = bankAccount.get("bank_account_verification_uri");
-		return uri === undefined || uri === null;
-	}),
-	hasBankAccountVerification: isAnyBankAccount("marketplaceBankAccounts.@each.bank_account_verification_uri", function(bankAccount) {
-		var uri = bankAccount.get("bank_account_verification_uri");
-		return !(uri === undefined || uri === null);
-	}),
+	isMarketplaceVerificationMissing: Ember.computed.not("isMarketplaceBankAccountsVerificationStarted"),
+	isMarketplaceBankAccountsVerificationStarted: function() {
+		var bankAccounts = this.get("marketplaceBankAccounts") || [];
+		return bankAccounts.any(function(bankAccount) {
+			return bankAccount.get("bank_account_verification_uri") !== null;
+		})
+	}.property("marketplaceBankAccounts.@each.bank_account_verification_uri"),
 
-	isBankAccountsEmpty: Ember.computed.equal('marketplaceBankAccounts.length', 0),
-	isNeedsStartVerification: function() {
-		return this.get("hasUnverifiedBankAccount") && !this.get("hasBankAccountVerification");
-	}.property("hasUnverifiedBankAccount", "hasBankAccountVerification"),
+	isBankAccountsEmpty: Ember.computed.and(
+		"isMarketplaceProduction",
+		"isMarketplaceBankAccountsLoaded",
+		"isMarketplaceBankAccountsEmpty"
+	),
 
-	isNeedsConfirmVerification: isAnyBankAccount("marketplaceBankAccounts.@each.can_confirm_verification", function(bankAccount) {
-		return bankAccount.get("can_confirm_verification");
-	}),
+	isNeedsStartVerification: Ember.computed.and(
+		"isMarketplaceProduction",
+		"isMarketplaceBankAccountsLoaded",
+		"isMarketplaceBankAccountsFull",
+		"isMarketplaceMissingDebitableBankAccount",
+		"isMarketplaceVerificationMissing"
+	),
+
+	isNeedsConfirmVerification: Ember.computed.and(
+		"isMarketplaceProduction",
+		"isMarketplaceBankAccountsLoaded",
+		"isMarketplaceBankAccountsFull",
+		"isMarketplaceMissingDebitableBankAccount",
+		"isMarketplaceBankAccountsVerificationStarted"
+	),
 });
 
 Balanced.NotificationMessageView = Balanced.View.extend({
