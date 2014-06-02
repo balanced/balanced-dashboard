8b477e8e77ca6c85104cc11e9f34e99b31b38639
Kyungmin Kim <kmin905@gmail.com>
2014-06-02 16:40:22 -0700

    Fix apply page

--- app/controllers/marketplaces.js
+++ app/controllers/marketplaces.js
@@ -1,4 +1,5 @@
 Balanced.MarketplacesController = Balanced.ArrayController.extend({
 	needs: ['marketplace', 'application', 'marketplaces'],
-	marketplaceBinding: 'controllers.marketplace'
+	marketplaceBinding: 'controllers.marketplace',
+	isApplyPage: Ember.computed.equal('controllers.application.currentRouteName', 'marketplaces.apply')
 });
