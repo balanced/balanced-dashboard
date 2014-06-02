6d84fd90bbd5764ec146e1337f1f6540c23a9cbf
Kyungmin Kim <kmin905@gmail.com>
2014-06-02 14:43:37 -0700

    Minor alignment tweaks

--- app/views/results.js
+++ app/views/results.js
@@ -165,7 +165,7 @@ Balanced.ResultsSortableColumnHeaderView = Balanced.View.extend({
 		var sortField = this.get('controller.sortField');
 		var sortOrder = this.get('controller.sortOrder');
 		var allowSortByNone = this.get('controller.allowSortByNone');
-		var nextSortOrder = 'asc';
+		var nextSortOrder = 'desc';
 
 		if (sortField === this.get('field')) {
 			switch (sortOrder) {
