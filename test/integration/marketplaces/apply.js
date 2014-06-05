8188bf54c7fd195c325577194e86016c96f6e7e9
Kyungmin Kim <kmin905@gmail.com>
2014-06-05 10:43:25 -0700

    Update test

--- test/integration/marketplaces/apply.js
+++ test/integration/marketplaces/apply.js
@@ -29,7 +29,7 @@ test('clicking business or personal shows data', function(assert) {
 		.click('a:contains("Business")')
 		.then(assertInputsCount(20))
 		.click('a:contains("Person")')
-		.then(assertInputsCount(15));
+		.then(assertInputsCount(18));
 });
 
 test('basic form validation and terms and conditions', function(assert) {
@@ -44,12 +44,12 @@ test('basic form validation and terms and conditions', function(assert) {
 		})
 		.click(submitButtonQuery)
 		.then(function() {
-			assert.equal($('.control-group.error').length, 15, 'expected error fields highlighted');
+			assert.equal($('.control-group.error').length, 18, 'expected error fields highlighted');
 		})
 		.click('#terms-and-conditions')
 		.click(submitButtonQuery)
 		.then(function() {
-			assert.equal($('.control-group.error').length, 14, 'expected error fields highlighted but not t&c');
+			assert.equal($('.control-group.error').length, 17, 'expected error fields highlighted but not t&c');
 		});
 });
 
