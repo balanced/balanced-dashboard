eeb408acff9017119096a50d2074b95b94257027
Kyungmin Kim <kmin905@gmail.com>
2014-06-05 16:26:45 -0700

    Format customer column in the customers table

--- app/views/tables/transactions_table_cell_view.js
+++ app/views/tables/transactions_table_cell_view.js
@@ -10,7 +10,6 @@ Balanced.TransactionCustomerCellView = Balanced.LinkedTwoLinesCellView.extend({
 	classNameBindings: [":account", "item.customer::null-field"],
 	primaryLabelText: Ember.computed.oneWay("item.customer_display_me"),
 	secondaryLabelText: Ember.computed.oneWay("item.customer_email"),
-	isBlank: Ember.computed.none('primaryLabelText', 'secondaryLabelText')
 });
 
 Balanced.TransactionAmountCellView = Balanced.LinkedTransactionCellView.extend({
