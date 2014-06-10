c3e49faaf8045e0e82b19daa6245a5088ed77948
Kyungmin Kim <kmin905@gmail.com>
2014-06-09 21:13:09 -0700

    Add links to transaction table cells and format customer name without email

--- app/views/funding_instrument/credit_funding_instrument_modal.js
+++ app/views/funding_instrument/credit_funding_instrument_modal.js
@@ -4,8 +4,9 @@ Balanced.CreditFundingInstrumentModalView = Balanced.FundingInstrumentModalView.
 	modalElement: '#credit-funding-instrument',
 
 	open: function() {
+		console.log(this.get('funding_instrument.uri') + '/credits')
 		var credit = Balanced.Credit.create({
-			uri: this.get('funding_instrument.credits_uri'),
+			uri: this.get('funding_instrument.uri') + '/credits',
 			source_uri: this.get('funding_instrument.uri'),
 			amount: null
 		});
