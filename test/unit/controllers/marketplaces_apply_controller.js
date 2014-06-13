643e83c72ecd7cefb3ecaffbe506d85691f2331a
cohitre <carlosrr@gmail.com>
2014-06-13 12:01:11 -0700

    Fixing test error by adding stub

--- test/unit/controllers/marketplaces_apply_controller.js
+++ test/unit/controllers/marketplaces_apply_controller.js
@@ -27,7 +27,8 @@ test("#save (valid request)", function(assert) {
 test("#save (invalid request)", function(assert) {
 	var productionAccessRequest = Ember.Object.create({
 		isValid: false,
-		validate: sinon.stub()
+		validate: sinon.stub(),
+		logValidationErrors: sinon.stub()
 	});
 	var subject = Balanced.MarketplacesApplyController.create();
 	subject.set("model", productionAccessRequest);
@@ -35,4 +36,5 @@ test("#save (invalid request)", function(assert) {
 	subject.send("save");
 
 	assert.ok(productionAccessRequest.validate.calledOnce);
+	assert.ok(productionAccessRequest.logValidationErrors.calledOnce);
 });
