5b7d18c2520109532b94b3422afc68d625e246e6
Kyungmin Kim <kmin905@gmail.com>
2014-06-25 15:35:48 -0700

    Add dispute fixture

--- test/integration/disputes.js
+++ test/integration/disputes.js
@@ -1,53 +1,49 @@
 module('Disputes', {
 	setup: function() {
-		Testing.setupMarketplace();
-		Testing.createDisputes();
+		Testing.useFixtureData();
 	},
 	teardown: function() {}
 });
 
-test('exist on the activity page', function(assert) {
-	var DISPUTES_ROUTE = Testing.MARKETPLACE_ROUTE + '/activity/disputes';
-	var activityDisputesPage = {
-		'table.disputes tbody tr:eq(0) td.date.initiated': 1,
-		'table.disputes tbody tr:eq(0) td.date.respond-by': 1,
-		'table.disputes tbody tr:eq(0) td.type': 'Pending',
-		'table.disputes tbody tr:eq(0) td.account': 1,
-		'table.disputes tbody tr:eq(0) td.funding-instrument': 1,
-		'table.disputes tbody tr:eq(0) td.amount': '$100.00',
-		'table.disputes tfoot td:eq(0)': 1
-	};
+test('can visit page', function(assert) {
+	var DISPUTES_ROUTE = Testing.FIXTURE_MARKETPLACE_ROUTE + '/disputes';
 
 	visit(DISPUTES_ROUTE)
 		.then(function() {
-			assert.ok($('table.disputes tbody tr').length >= 1, 'Correct # of Rows');
-
-			// Manually check the disputes uri is correct
-			var activityController = Balanced.__container__.lookup('controller:activity');
-			assert.equal(activityController.get('results_base_uri'), '/disputes', 'Disputes URI is correct');
-			assert.ok(activityController.get('results_uri').indexOf('sort=initiated_at') > 0, 'Disputes Sort is correct');
+			var disputesController = Balanced.__container__.lookup('controller:marketplace_disputes');
+			console.log(DISPUTES_ROUTE);
+			console.log(disputesController);
+			assert.equal(disputesController.get('results_base_uri'), '/disputes', 'Disputes URI is correct');
+			assert.ok(disputesController.get('results_uri').indexOf('sort=initiated_at') > 0, 'Disputes sort is correct');
 		})
-		.checkElements(activityDisputesPage, assert)
-		.click('table.disputes tfoot td.load-more-results a')
-		.then(function() {
-			assert.ok($('table.disputes tbody tr').length >= 3, 'has more disputes');
-		});
+		.checkElements({
+			"#content h1": "Disputes",
+			"#disputes table tbody tr": 1,
+			'table.disputes tbody tr:eq(0) td.date.initiated': 1,
+			'table.disputes tbody tr:eq(0) td.date.respond-by': 1,
+			'table.disputes tbody tr:eq(0) td.status': 'Pending',
+			'table.disputes tbody tr:eq(0) td.account': 1,
+			'table.disputes tbody tr:eq(0) td.funding-instrument': 1,
+			'table.disputes tbody tr:eq(0) td.amount': '$12.00',
+			'table.disputes tfoot td:eq(0)': 1
+		}, assert);
 });
 
-test('can visit page', function(assert) {
-	var disputePage = {
-		'#content h1': 'Dispute',
-		'#dispute > .main-header .title': 1, // 'Brand New Electric Guitar Rosewood Fingerboard Sunset Red',
-		'#dispute .customer-info .main-header .title': 1, // 'William Henry Cavendish III (whc@example.org)',
-		'#dispute .transaction-details .dispute .tt-title': 'Pending: $100.00',
-		'#dispute .transaction-details .debit .tt-title': 1, // 'Succeeded: $13.30',
-		'#dispute .evidence-portal-info': 1
-	};
+// test('can visit page', function(assert) {
+// 	var DISPUTE_ROUTE = DISPUTES_ROUTE + '/DT2xOc7zAdgufK4XsCIW5QgD';
+// 	var disputePage = {
+// 		'#content h1': 'Dispute',
+// 		'#dispute > .main-header .title': 1, // 'Brand New Electric Guitar Rosewood Fingerboard Sunset Red',
+// 		'#dispute .customer-info .main-header .title': 1, // 'William Henry Cavendish III (whc@example.org)',
+// 		'#dispute .transaction-details .dispute .tt-title': 'Pending: $100.00',
+// 		'#dispute .transaction-details .debit .tt-title': 1, // 'Succeeded: $13.30',
+// 		'#dispute .evidence-portal-info': 1
+// 	};
 
-	visit(Testing.DISPUTE_ROUTE)
-		.checkElements(disputePage, assert)
-		.click('#dispute .evidence-portal-info a')
-		.then(function() {
-			// Hey
-		});
-});
+// 	visit(Testing.DISPUTE_ROUTE)
+// 		.checkElements(disputePage, assert)
+// 		.click('#dispute .evidence-portal-info a')
+// 		.then(function() {
+// 			assert.ok($('.modal-header h2'), 'Attach docs');
+// 		});
+// });
---
+++ test/support/fixtures/disputes.js
@@ -0,0 +1,47 @@
+Balanced.Adapter.addFixtures([{
+	"meta": {
+		"last": "/disputes?sort=initiated_at%2Cdesc&offset=0&q=&limit=50&created_at%5B%3C%5D=2014-06-26T00%3A00%3A00.000Z&type=dispute&created_at%5B%3E%5D=2014-01-22T23%3A00%3A24.645Z",
+		"next": null,
+		"href": "/disputes?sort=initiated_at%2Cdesc&offset=0&q=&limit=50&created_at%5B%3C%5D=2014-06-26T00%3A00%3A00.000Z&type=dispute&created_at%5B%3E%5D=2014-01-22T23%3A00%3A24.645Z",
+		"limit": 50,
+		"offset": 0,
+		"previous": null,
+		"total": 2,
+		"first": "/disputes?sort=initiated_at%2Cdesc&offset=0&q=&limit=50&created_at%5B%3C%5D=2014-06-26T00%3A00%3A00.000Z&type=dispute&created_at%5B%3E%5D=2014-01-22T23%3A00%3A24.645Z"
+	},
+	"links": {
+		"disputes.events": "/disputes/{disputes.id}/events",
+		"disputes.transaction": "/resources/{disputes.transaction}"
+	},
+	"disputes": [{
+		"status": "pending",
+		"links": {
+			"transaction": "WD25YaEwlEeYDXPmUmUEslAp"
+		},
+		"respond_by": "2014-07-25T18:48:17.213582Z",
+		"updated_at": "2014-06-25T18:48:27.435284Z",
+		"created_at": "2014-06-25T18:48:27.435275Z",
+		"reason": "fraud",
+		"id": "DT2i1GU1dayFu9xx9VA5cB5p",
+		"currency": "USD",
+		"amount": 200,
+		"meta": {},
+		"href": "/disputes/DT2i1GU1dayFu9xx9VA5cB5p",
+		"initiated_at": "2014-06-25T18:48:17.213579Z"
+	}, {
+		"status": "pending",
+		"links": {
+			"transaction": "WD2e4R5qiBO754zsgNg0xizv"
+		},
+		"respond_by": "2014-07-16T23:09:37.214304Z",
+		"updated_at": "2014-06-16T23:09:54.287539Z",
+		"created_at": "2014-06-16T23:09:54.287535Z",
+		"reason": "fraud",
+		"id": "DT2xOc7zAdgufK4XsCIW5QgD",
+		"currency": "USD",
+		"amount": 1200,
+		"meta": {},
+		"href": "/disputes/DT2xOc7zAdgufK4XsCIW5QgD",
+		"initiated_at": "2014-06-16T23:09:37.214301Z"
+	}]
+}]);
