dd798a93934ede9fd568ceaa323b0fe2a1026adf
cohitre <carlosrr@gmail.com>
2014-06-25 14:56:55 -0700

    Handling logs table sort

--- app/controllers/marketplace/marketplace_logs_controller.js
+++ app/controllers/marketplace/marketplace_logs_controller.js
@@ -1,7 +1,15 @@
 Balanced.MarketplaceLogsController = Balanced.ObjectController.extend(Ember.Evented, {
 	needs: ['marketplace'],
 	resultsLoader: Ember.computed.oneWay("model"),
+	sortField: Ember.computed.oneWay("resultsLoader.sortField"),
+	sortOrder: Ember.computed.oneWay("resultsLoader.sortDirection"),
 	actions: {
+		changeSortOrder: function(field, direction) {
+			this.get("resultsLoader").setProperties({
+				sortField: field,
+				sortDirection: direction
+			});
+		},
 		changeDateFilter: function(startTime, endTime) {
 			this.get("resultsLoader").setProperties({
 				endTime: endTime,
