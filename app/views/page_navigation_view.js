5b918361532c7e92a65e9bedb15c6146780c21ca
cohitre <carlosrr@gmail.com>
2014-07-18 14:09:13 -0700

    Adding page navigations to views

--- app/views/page_navigation_view.js
+++ app/views/page_navigation_view.js
@@ -1,10 +1,26 @@
 Balanced.PageNavigationView = Ember.View.extend({
-	templateName: 'page_navigation',
+	layoutName: "page_navigations/page_navigation_layout",
+});
+
+Balanced.TransactionsPageNavigationView = Balanced.PageNavigationView.extend({
+	templateName: 'page_navigations/page_navigation_actions',
+	title: "Transactions",
+	actionButtons: function() {
+		return [{
+			modalViewName: Balanced.DebitNewFundingInstrumentModalView,
+			buttonName: 'Debit a card'
+		}, {
+			modalViewName: Balanced.CreditNewFundingInstrumentModalView,
+			buttonName: 'Credit a bank account'
+		}];
+	}.property()
 
 });
 
 Balanced.CustomerPageNavigationView = Balanced.PageNavigationView.extend({
+	templateName: 'page_navigations/page_navigation_actions',
 	pageType: 'Customer',
+	title: Ember.computed.oneWay("model.display_me"),
 	actionClassNames: ['credit-customer'],
 	actionButtons: function() {
 		return [{
