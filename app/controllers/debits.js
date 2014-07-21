6cfadc41bb218a0166b8693993aea816c7fa3ada
cohitre <carlosrr@gmail.com>
2014-07-21 09:14:58 -0700

    Converting transaction pages to use TransactionsPageNavigationView.

--- app/controllers/debits.js
+++ app/controllers/debits.js
@@ -1,11 +1,3 @@
 Balanced.DebitsController = Balanced.ObjectController.extend({
 	needs: ['marketplace'],
-	pageType: function() {
-		return Balanced.Utils.capitalize(this.get("model.type_name"));
-	}.property("model.type_name"),
-
-	pageTitle: function() {
-		return Balanced.Utils.formatCurrency(this.get("model.amount"));
-	}.property("model.amount"),
-
 });
--- app/views/page_navigation_view.js
+++ app/views/page_navigation_view.js
@@ -1,3 +1,13 @@
 Balanced.PageNavigationView = Ember.View.extend({
 	layoutName: "page_navigations/page_navigation_layout",
 });
+
+Balanced.TransactionPageNavigationView = Balanced.PageNavigationView.extend({
+	pageType: function() {
+		return Balanced.Utils.capitalize(this.get("model.type_name"));
+	}.property("model.type_name"),
+
+	title: function() {
+		return Balanced.Utils.formatCurrency(this.get("model.amount"));
+	}.property("model.amount"),
+});
