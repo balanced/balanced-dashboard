55eb3ed2e06e7c54d773137e76650deef2d745e4
cohitre <carlosrr@gmail.com>
2014-07-21 09:05:03 -0700

    using pagenavigationview on debits page

--- app/controllers/debits.js
+++ app/controllers/debits.js
@@ -1,3 +1,11 @@
 Balanced.DebitsController = Balanced.ObjectController.extend({
-	needs: ['marketplace']
+	needs: ['marketplace'],
+	pageType: function() {
+		return Balanced.Utils.capitalize(this.get("model.type_name"));
+	}.property("model.type_name"),
+
+	pageTitle: function() {
+		return Balanced.Utils.formatCurrency(this.get("model.amount"));
+	}.property("model.amount"),
+
 });
