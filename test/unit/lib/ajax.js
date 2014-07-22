f95dd4241ff8d40cadb9921be6de5b00ea2f2bf9
cohitre <carlosrr@gmail.com>
2014-07-21 17:41:18 -0700

    fixing csrf token

--- test/unit/lib/ajax.js
+++ test/unit/lib/ajax.js
@@ -1,17 +1,16 @@
-module('Balanced.NET.', {
-	setup: function() {
-		Testing.stop();
-		Balanced.NET.loadCSRFToken().then(function() {
+module('Balanced.NET.');
+
+asyncTest('loadCSRFTokenIfNotLoaded', function(assert) {
+	Testing.stop();
+	Balanced.NET.csrfToken = undefined;
+	Balanced.NET.loadCSRFToken()
+		.then(function() {
+			var existingToken = Balanced.NET.csrfToken;
+
+			assert.ok(existingToken && existingToken.length > 0, 'Already has CSRF token');
+			Balanced.NET.loadCSRFTokenIfNotLoaded();
+
+			assert.equal(existingToken, Balanced.NET.csrfToken, 'Did not get new CSRF token');
 			Testing.start();
 		});
-	}
-});
-
-test('loadCSRFTokenIfNotLoaded', function(assert) {
-	var existingToken = Balanced.NET.csrfToken;
-
-	assert.ok(existingToken && existingToken.length > 0, 'Already has CSRF token');
-	Balanced.NET.loadCSRFTokenIfNotLoaded();
-
-	assert.equal(existingToken, Balanced.NET.csrfToken, 'Did not get new CSRF token');
 });
