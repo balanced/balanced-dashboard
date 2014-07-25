4752ebe2011316c99ce3cbd7ed8ab043b6301d89
cohitre <carlosrr@gmail.com>
2014-07-24 17:10:28 -0700

    disabling redirect to last MP

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,6 @@ Balanced.IndexRoute = Balanced.AuthRoute.extend({
 	redirect: function() {
 		var self = this;
 		var marketplaceUri = this.get('auth').getLastUsedMarketplaceUri();
-		if (marketplaceUri) {
-			Balanced.Marketplace.find(marketplaceUri)
-				.then(function(marketplace) {
-					self.transitionTo('transaction', marketplace);
-				});
-		} else {
-			this.transitionTo('marketplaces');
-		}
+		this.transitionTo('marketplaces');
 	}
 });
