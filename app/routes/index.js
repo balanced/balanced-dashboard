a2e08b0f6656434bae933368884bba8506a3f90a
cohitre <carlosrr@gmail.com>
2014-07-24 17:33:56 -0700

    Breaking up order transactions into multiple tables

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,5 @@
 Balanced.IndexRoute = Balanced.AuthRoute.extend({
 	redirect: function() {
-		var self = this;
-		var marketplaceUri = this.get('auth').getLastUsedMarketplaceUri();
 		this.transitionTo('marketplaces');
 	}
 });
