91917519bdca1ce4011a4f91a3825a7704e3319a
cohitre <carlosrr@gmail.com>
2014-08-04 22:27:18 -0700

    Closing search on link click

--- app/views/search_bar_view.js
+++ app/views/search_bar_view.js
@@ -4,6 +4,14 @@ Balanced.SearchBarView = Balanced.View.extend({
 	overlayClass: 'overlaid',
 	isHighlight: false,
 
+	didInsertElement: function() {
+		this._super();
+		var self = this;
+		this.$().on("click", ".results tbody a", function() {
+			self.get("controller").send("closeSearch");
+		});
+	},
+
 	mouseEnter: function(evt) {
 		this.set("isHighlight", true);
 	},
