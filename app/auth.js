731bff599b5f150083b8c788291a6a914b13aa69
cohitre <carlosrr@gmail.com>
2014-08-07 14:27:48 -0700

    linking to claim page for non-users

--- app/auth.js
+++ app/auth.js
@@ -243,6 +243,13 @@ var auth = Balanced.Auth = Ember.Namespace.extend(Ember.Evented).create({
 			});
 	},
 
+	isUnregistered: function() {
+		var isUserEmpty = Ember.isNone(this.get("user"));
+		var isUserGuest = !!this.get("isGuest");
+		console.log(isUserEmpty, isUserGuest);
+		return isUserEmpty || isUserGuest;
+	},
+
 	setAuthProperties: function(signedIn, user, userId, authToken, isGuest, isAdmin) {
 		this.setProperties({
 			signedIn: signedIn,
