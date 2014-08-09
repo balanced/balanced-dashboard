5bee9b92bc8010f6f747d2fc157d501d626e2742
cohitre <carlosrr@gmail.com>
2014-08-08 17:00:33 -0700

    Cleaning claim process

---
+++ app/models/factories/user_account_factory.js
@@ -0,0 +1,43 @@
+Balanced.UserAccountFactory = Balanced.BaseFactory.extend({
+	validations: {
+		email_address: {
+			presence: true
+		},
+		password: {
+			presence: true,
+			length: {
+				minimum: 6
+			}
+		},
+		passwordConfirm: {
+			presence: true,
+			matches: {
+				validator: function(object, attribute, value) {
+					var password = object.get('password');
+					if (value !== password) {
+						object.get('validationErrors').add(attribute, 'invalid');
+					}
+				}
+			}
+		}
+	},
+
+	getPostUrl: function() {
+		return "%@/users".fmt(ENV.BALANCED.AUTH);
+	},
+	getPostAttributes: function() {
+		return this.getProperties(
+			"email_address",
+			"password",
+			"passwordConfirm"
+		);
+	},
+	handleResponse: function(response) {
+		return response.uri;
+	},
+	handleErrorResponse: function() {
+		var validationErrors = this.get("validationErrors");
+		validationErrors.clear();
+		validationErrors.add("", "serverError", null, "There was an error creating your account.");
+	},
+});
--- app/views/register_flow/user_create_modal_view.js
+++ app/views/register_flow/user_create_modal_view.js
@@ -1,52 +1,61 @@
 var Full = Balanced.Modals.FullModalMixin;
-var Save = Balanced.Modals.ObjectValidateAndSaveMixin;
 
-Balanced.UserCreateModalView = Balanced.ModalBaseView.extend(Full, Save, {
+Balanced.UserCreateModalView = Balanced.ModalBaseView.extend(Full, {
 	templateName: "register_flow/user_create_modal",
 	title: "Create your account",
 
-	bindUserMarketplace: function(user) {
-		var authToken = Balanced.Auth.get('authToken');
-		var apiKeysUri = user.get("api_keys_uri");
+	auth: Balanced.Auth,
 
-		if (authToken) {
-			return Balanced.UserMarketplace
-				.create({
-					uri: apiKeysUri,
-					secret: authToken
-				})
-				.save()
-				.then(function() {
-					return user.reload();
+	model: function() {
+		return Balanced.UserAccountFactory.create();
+	}.property(),
+
+	save: function(model) {
+		model.validate();
+		if (model.get("isValid")) {
+			return model.save()
+				.then(function(userUri) {
+					return Ember.RSVP.resolve(userUri);
+				}, function(errors) {
+					return Ember.RSVP.reject(errors);
 				});
+		} else {
+			return Ember.RSVP.reject();
 		}
 	},
 
+	isSaving: false,
 	actions: {
 		save: function() {
-			var self = this;
-			var model = this.get("model");
 			var controller = this.get("controller");
 
+			var self = this;
+			var model = this.get("model");
+			var authToken = this.get("auth.authToken");
+
 			this.save(model)
-				.then(function() {
+				.then(function(userUri) {
 					return Balanced.Auth.signIn(model.get('email_address'), model.get('passwordConfirm'));
 				})
-				.then(function() {
-					return self.bindUserMarketplace(model);
+				.then(function(user) {
+					var apiKeysUri = user.get("api_keys_uri");
+
+					if (authToken) {
+						return Balanced.UserMarketplace
+							.create({
+								uri: apiKeysUri,
+								secret: authToken
+							})
+							.save()
+							.then(function() {
+								return user.reload();
+							});
+					}
 				})
 				.then(function() {
+					self.close();
 					return controller.transitionToRoute('index');
 				});
 		}
-	}
-});
-
-Balanced.UserCreateModalView.reopenClass({
-	open: function() {
-		var claim = Balanced.Claim.create();
-		return this.create({
-			model: claim
-		});
 	}
 });
