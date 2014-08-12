16fcd96a2b23973ef32385abf6148aa6973c5c51
cohitre <carlosrr@gmail.com>
2014-08-12 10:16:35 -0700

    Updating how we're deleting marketplaces

--- app/controllers/marketplaces/index.js
+++ app/controllers/marketplaces/index.js
@@ -5,9 +5,6 @@ Balanced.MarketplacesIndexController = Balanced.ArrayController.extend(Ember.Eve
 	productionMarketplaces: Ember.computed.filterBy("auth.user.user_marketplaces", "production", true),
 
 	actions: {
-		promptToDeleteMarketplace: function(marketplace) {
-			this.trigger('openDeleteMarketplaceModal', marketplace);
-		},
 		goToMarketplace: function(marketplace) {
 			this.transitionTo('marketplace', marketplace);
 		}
--- app/routes/marketplaces/apply.js
+++ app/routes/marketplaces/apply.js
@@ -1,12 +1,9 @@
 Balanced.MarketplacesApplyRoute = Balanced.Route.extend({
-	title: 'Apply for production access',
-
-	setupController: function(controller) {
-		var model = Balanced.ProductionAccessRequest.create({});
-
-		model.set("user", this.get("user"));
-
-		this._super(controller, model);
-		this.controllerFor('marketplace').set('content', null);
+	renderTemplate: function() {
+		var self = this;
+		this.render("marketplaces.index");
+		Ember.run.next(function() {
+			self.send("openModal", Balanced.ApiKeyCreateModalView);
+		});
 	},
 });
