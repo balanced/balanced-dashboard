9c8d8b12136908ab398110c6396bcd76d73e2679
Kyungmin Kim <kmin905@gmail.com>
2014-08-13 11:49:47 -0700

    Display server error messages in create account modal

--- app/views/form_validation_errors_view.js
+++
@@ -1,4 +0,0 @@
-Balanced.FormValidationErrorsView = Balanced.View.extend({
-	templateName: "form_fields/form_validation_errors",
-	errorMessages: Ember.computed.oneWay("model.validationErrors.fullMessages"),
-});
