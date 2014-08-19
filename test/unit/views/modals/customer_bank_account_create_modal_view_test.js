a524dc8cad3863c8981b11111da54ac8be8a34de
cohitre <carlosrr@gmail.com>
2014-08-19 00:49:23 -0700

    Removing this test while it gets rewritten

--- test/unit/views/modals/customer_bank_account_create_modal_view_test.js
+++ test/unit/views/modals/customer_bank_account_create_modal_view_test.js
@@ -5,36 +5,15 @@ test("#model", function(assert) {
 	assert.equal(subject.get("model.constructor"), Balanced.BankAccount);
 });
 
-asyncTest("#save invalid", function(assert) {
-	var view = Balanced.Modals.CustomerBankAccountCreateModalView.create();
-	view.get("model").setProperties({
-		account_number: "xxxxxxx",
-		routing_number: "xxxxxxx",
-	});
-	view.save()
-		.then(undefined, function() {
-			var errors = view.get("model.validationErrors");
-			assert.deepEqual(errors, {
-				"name": "Invalid field [name] - Missing field \"name\"",
-				"routing_number": "Invalid field [routing_number] - \"xxxxxxx\" is not a valid routing number"
-			});
-		})
-		.then(start);
-});
-
 asyncTest("#save", function(assert) {
 	var model = Ember.Object.create({
 		tokenizeAndCreate: sinon.stub(),
 		route_name: "cool_route"
 	});
 	model.tokenizeAndCreate.returns(Ember.RSVP.resolve(model));
-	var controller = {
-		transitionToRoute: sinon.stub()
-	};
 
 	var view = Balanced.Modals.CustomerBankAccountCreateModalView.create({
 		model: model,
-		controller: controller,
 		customer: {
 			id: "xxxxxxxxxxxxxxx"
 		}
@@ -43,6 +22,5 @@ asyncTest("#save", function(assert) {
 		assert.ok(model.tokenizeAndCreate.calledOnce);
 		assert.deepEqual(model.tokenizeAndCreate.firstCall.args, ["xxxxxxxxxxxxxxx"]);
 		start();
-
 	});
 });
