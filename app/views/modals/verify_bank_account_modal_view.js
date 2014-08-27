e1cfb9e1522f94bf8f55cc9beec09af623f5ae24
Kyungmin Kim <kmin905@gmail.com>
2014-08-27 15:46:41 -0700

    Display error message when there is a pending verification for bank account

--- app/views/modals/verify_bank_account_modal_view.js
+++ app/views/modals/verify_bank_account_modal_view.js
@@ -5,9 +5,11 @@ Balanced.Modals.VerifyBankAccountModalView = Balanced.ModalBaseView.extend({
 	title: "Bank account verification",
 
 	isSaving: false,
+
 	actions: {
 		save: function() {
 			var self = this;
+			var controller = this.get("container").lookup("controller:modal_notification_center");
 			var verification = Balanced.Verification.create({
 				uri: this.get('bankAccount.bank_account_verifications_uri')
 			});
@@ -20,7 +22,8 @@ Balanced.Modals.VerifyBankAccountModalView = Balanced.ModalBaseView.extend({
 					self.get("bankAccount").reload();
 					self.get("container").lookup("controller:marketplace").updateBankAccountNotifications();
 					self.close();
-				}, function() {
+				}, function(response) {
+					controller.alertError(response.errors[0].additional);
 					self.set("isSaving", false);
 				});
 		}
