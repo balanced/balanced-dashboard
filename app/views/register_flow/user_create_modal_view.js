3dec6186451a225bbf50bca5decf18953a65a5df
Kyungmin Kim <kmin905@gmail.com>
2014-08-26 18:16:55 -0700

    Add next step page

--- app/views/register_flow/user_create_modal_view.js
+++ app/views/register_flow/user_create_modal_view.js
@@ -41,7 +41,7 @@ Balanced.UserCreateModalView = Balanced.RegisterFlowBaseModal.extend({
 
 	actions: {
 		nextStep: function(marketplace) {
-			this.openNext(Balanced.ApiKeyCreateModalView);
+			this.openNext(Balanced.NextStepView);
 			var controller = this.getModalNotificationController();
 			controller.alertSuccess("Login created");
 		},
