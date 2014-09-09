dab0e9d2f0c07aed50a2bc4fddbb8d5a19be2f2d
Kyungmin Kim <kmin905@gmail.com>
2014-09-08 17:08:44 -0700

    Add maxlength for evidence note

--- app/views/form_fields/textarea_form_field_view.js
+++ app/views/form_fields/textarea_form_field_view.js
@@ -1,3 +1,14 @@
 Balanced.TextAreaFormFieldView = Balanced.BaseFormFieldView.extend({
-	templateName: "form_fields/textarea_form_field"
+	templateName: "form_fields/textarea_form_field",
+	maxlength: 0,
+	explanationText: function() {
+		var maxLength = this.get('maxlength');
+
+		if (maxLength > 0) {
+			var noteLength = this.get('model.note') ? this.get('model.note.length') : 0;
+			var remaining = maxLength - noteLength;
+			return "%@ characters remaining".fmt(remaining);
+		}
+	}.property('model.note.length'),
+
 });
