3c260eef955de192948526f62997ea93f9b46a7e
Kyungmin Kim <kmin905@gmail.com>
2014-09-09 16:04:59 -0700

    Fix date table header alignment

--- app/views/sidebar/marketplace_sidebar_view.js
+++ app/views/sidebar/marketplace_sidebar_view.js
@@ -26,7 +26,8 @@ var SIDEBAR_ITEMS = [{
 	linkText: "Disputes",
 	linkIcon: "icon-disputes",
 	routeName: "marketplace.disputes",
-	isSelectedBinding: "controller.marketplace.disputeSelected"
+	isSelectedBinding: "controller.marketplace.disputeSelected",
+	alertCount: "3"
 }, {
 	linkText: "Logs",
 	linkIcon: "icon-logs",
