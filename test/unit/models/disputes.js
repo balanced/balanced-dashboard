e5f7c71ec442a48c5667acd08e855e54011a85ca
Marshall Jones <marshall@balancedpayments.com>
2014-09-17 18:22:09 -0700

    test dispute state logic

---
+++ test/unit/models/disputes.js
@@ -0,0 +1,25 @@
+module('Balanced.Dispute');
+
+test('state', function(assert) {
+	var dispute = Balanced.Dispute.create();
+	var document = Balanced.DisputeDocument.create();
+	dispute.set('documents', [document]);
+
+	[{
+		actual: 'won',
+		expected: 'won'
+	}, {
+		actual: 'pending',
+		expected: 'submitted'
+	}, {
+		actual: 'lost',
+		expected: 'lost'
+	}].each(function(state) {
+		dispute.set('status', state.actual);
+		assert.equal(dispute.get('state', state.expected));
+	});
+
+	dispute.set('documents', []);
+	dispute.set('status', 'pending');
+	assert.equal(dispute.get('state', 'pending'));
+});
