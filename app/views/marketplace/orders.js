3c574780165c8b0aaa5f50d77a9862d3dd7c6391
Kyungmin Kim <kmin905@gmail.com>
2014-10-29 14:30:02 +0900

    Use bootstrap grid for cards layout

--- app/views/marketplace/orders.js
+++ app/views/marketplace/orders.js
@@ -1,9 +1,6 @@
 var MarketplaceOrdersView = Ember.View.extend({
 	layoutName: "marketplace/payments-layout",
 	templateName: "marketplace/orders",
-	didInsertElement: function () {
-		$('#content, #content > div.ember-view:last-child').height("100%");
-	}
 });
 
 export default MarketplaceOrdersView;
--- app/views/tables/cells/linked-card.js
+++ app/views/tables/cells/linked-card.js
@@ -3,7 +3,7 @@ import Ember from "ember";
 var LinkedCardView = Ember.View.extend({
 	tagName: "div",
 	templateName: "tables/cells/linked-card",
-	classNames: ["card"],
+	classNames: ["card", "col-md-3"],
 	// title: Ember.computed.oneWay("title")
 });
 
