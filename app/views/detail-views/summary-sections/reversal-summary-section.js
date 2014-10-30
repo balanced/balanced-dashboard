34b2293100188c6c2c408f957bef907ef2ca22e5
Kyungmin Kim <kmin905@gmail.com>
2014-10-30 16:56:34 +0900

    Fix failing tests

--- app/views/detail-views/summary-sections/reversal-summary-section.js
+++ app/views/detail-views/summary-sections/reversal-summary-section.js
@@ -4,8 +4,8 @@ var ReversalSummarySectionView = SummarySectionView.extend({
 	statusText: Ember.computed.alias('model.status_description'),
 
 	linkedResources: function() {
-		return this.resourceLinks("model.credit.customer", "model.credit.destination");
-	}.property("model.credit.customer", "model.credit.destination")
+		return this.resourceLinks("model.description", "model.credit.customer", "model.credit.destination");
+	}.property("model.description", "model.credit.customer", "model.credit.destination")
 });
 
 export default ReversalSummarySectionView;
--- tests/integration/orders-test.js
+++ tests/integration/orders-test.js
@@ -17,84 +17,7 @@ module('Integration - Order Page', {
 		Testing.setupMarketplace();
 
 		andThen(function() {
-			Models.Order.create({
-				uri: '/customers/' + Testing.CUSTOMER_ID + '/orders',
-				description: '#123'
-			}).save().then(function(order) {
-				Testing.ORDER_ID = order.get('id');
-				Testing.ORDER_ROUTE = '/marketplaces/' + Testing.MARKETPLACE_ID +
-					'/orders/' + Testing.ORDER_ID;
-
-				return Models.Debit.create({
-					uri: '/customers/' + Testing.CUSTOMER_ID + '/debits',
-					appears_on_statement_as: 'Pixie Dust',
-					amount: 10000,
-					description: 'Cocaine',
-					links: {
-						order: '/orders/' + Testing.ORDER_ID
-					}
-				}).save();
-			}).then(function(debit) {
-				Testing.DEBIT = debit;
-				return Testing._createBankAccount();
-			}).then(function(bankAccount) {
-				Testing.BANK_ACCOUNT_ID = bankAccount.get('id');
-
-				return Models.Credit.create({
-					uri: '/bank_accounts/' + Testing.BANK_ACCOUNT_ID + '/credits',
-					amount: 1000,
-					links: {
-						order: '/orders/' + Testing.ORDER_ID
-					}
-				}).save();
-			}).then(function(credit) {
-				Testing.CREDIT_1 = credit;
-
-				return Models.BankAccount.create({
-					uri: '/customers/' + Testing.CUSTOMER_ID + '/bank_accounts',
-					name: 'Test Account 2',
-					account_number: '12345',
-					routing_number: '122242607',
-					type: 'checking'
-				}).save();
-			}).then(function(bankAccount) {
-				Testing.BANK_ACCOUNT_ID = bankAccount.get('id');
-
-				return Models.Credit.create({
-					uri: '/bank_accounts/' + Testing.BANK_ACCOUNT_ID + '/credits',
-					amount: 1000,
-					links: {
-						order: '/orders/' + Testing.ORDER_ID
-					}
-				}).save();
-			}).then(function(credit) {
-				Testing.CREDIT_2 = credit;
-
-				return Models.Refund.create({
-					uri: '/debits/' + Testing.DEBIT.get('id') + '/refunds',
-					amount: 200,
-					description: 'Cocaine refund',
-					links: {
-						order: '/orders/' + Testing.ORDER_ID
-					}
-				}).save();
-			}).then(function(refund) {
-				Testing.REFUND = refund;
-
-				return Models.Reversal.create({
-					uri: '/credits/' + Testing.CREDIT_1.get('id') + '/reversals',
-					amount: 100,
-					links: {
-						order: '/orders/' + Testing.ORDER_ID
-					}
-				}).save();
-			}).then(function(reversal) {
-				Testing.REVERSAL = reversal;
-
-				return Models.Customer.find('/customers/' + Testing.CUSTOMER_ID);
-			}).then(function(customer) {
-				Testing.CUSTOMER = customer;
-			});
+			return Testing.createOrder();
 		});
 	},
 	teardown: function() {
@@ -108,12 +31,6 @@ var assertQueryString = function(string, expected) {
 		deepEqual(qsParameters[key], value, "Query string parameter %@".fmt(key));
 	});
 };
-
-test('can visit order page', function() {
-	visit(Testing.ORDER_ROUTE)
-		.checkPageType("OrderOrder escrow")
-		.checkPageTitle("#123");
-});
 
 test("can visit orders page", function() {
 	visit(Testing.MARKETPLACE_ROUTE)
@@ -129,3 +46,20 @@ test("can visit orders page", function() {
 			});
 		});
 });
+
+test('can visit order page', function() {
+	visit(Testing.ORDER_ROUTE)
+		.checkPageType("OrderOrder escrow")
+		.checkPageTitle("#123");
+});
+
+test('displays correct number of charges and payouts per customer', function() {
+	visit(Testing.ORDER_ROUTE)
+		.checkElements({
+			".customer-group": 2,
+			".grouped-transactions-container": 2,
+			".grouped-transactions-container .grouped-transactions": 3,
+			".grouped-transactions-container .grouped-transactions tr": 5
+		});
+});
+
