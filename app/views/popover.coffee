3961cf0d28cc65d9f9452b179940204eb8e25c9a
Kyungmin Kim <kmin905@gmail.com>
2014-11-05 18:48:39 +0900

    Address damon's feedback on orders

--- app/views/popover.coffee
+++ app/views/popover.coffee
@@ -4,9 +4,12 @@ PopoverView = Ember.View.extend
 	tagName: 'a'
 	attributeBindings: ['class', 'data-toggle', 'data-placement', 'data-original-title', 'data-content', 'data-html', 'data-trigger', 'data-template']
 	'data-toggle': 'popover'
+	selector: '[rel="popover"]'
 
 	didInsertElement: ->
-		@$().popover()
+		@$().popover().parent().on 'click', '.close', (e) =>
+    		$(e.target).parents(".popover").popover('hide')
+
 		@_super()
 
 `export default PopoverView;`
--- app/views/tables/cells/transaction/unlinked-status-cell.js
+++ app/views/tables/cells/transaction/unlinked-status-cell.js
@@ -6,14 +6,16 @@ var UnlinkedStatusCellView = LinkedTextCellView.extend({
 	iconName: "icon-unlinked",
 	tooltipTitle: "This transaction is unlinked",
 	tooltipContent: function () {
+		var closeButton = '<a class="close"><i class="icon-x"></i></a>';
 		var linkTemplate = '<a class="linked-list" href="%@" target="_blank"><i class="%@ non-interactive"></i>%@</a>';
 
 		var bodyText = "<p>Balanced requires that all debits must be attributed to the correct merchant and corresponding credit. To meet this requirement, you'll need to link transactions using the Order resource from the 1.1 version of the Balanced API. Click on the links below to learn more.<p>";
 		bodyText += linkTemplate.fmt("https://docs.balancedpayments.com/1.1/guides/orders/", "icon-payments", "How an Order works");
 		bodyText += linkTemplate.fmt("https://docs.balancedpayments.com/1.1/guides/migrating-api-1-1/", "icon-code", "How to implement Orders in API v1.1");
+		bodyText += closeButton;
 
 		return bodyText;
-	}.property()
+	}.property(),
 });
 
 export default UnlinkedStatusCellView;
