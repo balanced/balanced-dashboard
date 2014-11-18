0c779ece14e1037e8c16014c1c36d98f06d76b45
cohitre <carlosrr@gmail.com>
2014-11-18 15:38:57 -0800

    Adding missing internal description changes

--- app/views/detail-views/summary-sections/credit-summary-section.js
+++ app/views/detail-views/summary-sections/credit-summary-section.js
@@ -4,8 +4,8 @@ var CreditSummarySectionView = SummarySectionView.extend({
 	statusText: Ember.computed.alias('model.status_description'),
 
 	linkedResources: function() {
-		return this.resourceLinks("model.customer", "model.destination");
-	}.property("model.customer", "model.destination")
+		return this.resourceLinks("model.description", "model.customer", "model.destination");
+	}.property("model.customer", "model.destination", "model.description")
 });
 
 export default CreditSummarySectionView;
--- app/views/detail-views/summary-sections/hold-summary-section.js
+++ app/views/detail-views/summary-sections/hold-summary-section.js
@@ -2,8 +2,8 @@ import SummarySectionView from "./summary-section";
 
 var HoldSummarySectionView = SummarySectionView.extend({
 	linkedResources: function() {
-		return this.resourceLinks("model.customer", "model.source");
-	}.property("model.customer", "model.source")
+		return this.resourceLinks("model.description", "model.customer", "model.source");
+	}.property("model.customer", "model.source", "model.description")
 });
 
 export default HoldSummarySectionView;
