5882967589a74182110c9f283d06608f09094641
Kyungmin Kim <kmin905@gmail.com>
2014-11-26 11:29:05 -0800

    Fix search results dropdown filter

--- app/views/results/search-results-dropdown-filter.js
+++ app/views/results/search-results-dropdown-filter.js
@@ -2,7 +2,22 @@ import ResultsDropdownFilterView from "./results-dropdown-filter";
 import { defineFilter } from "./results-dropdown-filter";
 
 var SearchResultsDropdownFilterView = ResultsDropdownFilterView.extend({
-	templateName: "results/search-results-dropdown-filter",
+	resetFilters: function() {
+		var filters = this.get("filters");
+
+		filters.map(function(filter) {
+			filter.set("isSelected", filter.get('text') === "All");
+		});
+		this.set("isSelected", false);
+	}.observes("parentView.loader"),
+
+	actions: {
+		setFilter: function(filterLink) {
+			this.toggleSelected(filterLink);
+			// this.get("parentView.resultsLoader").send(this.get("actionName"), filterLink.value);
+			this.get("parentView").send(this.get("actionName"), filterLink.value);
+		}
+	}
 });
 
 export default SearchResultsDropdownFilterView;
