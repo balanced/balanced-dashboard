a600094acfa3447842ceaf902011e9b773732b74
Kyungmin Kim <kmin905@gmail.com>
2014-12-01 10:34:27 -0800

    Update radio input style

--- app/views/form-fields/radio-form-field.js
+++ app/views/form-fields/radio-form-field.js
@@ -3,12 +3,6 @@ import BaseFormFieldView from './base-form-field';
 var RadioFormFieldView = BaseFormFieldView.extend({
 	templateName: "form-fields/radio-form-field",
 	inputName: "radio",
-
-	// updateParentValue: function() {
-	// 	var value = this.get("value");
-	// 	console.log(this.getProperties("value", "model", "fieldName"))
-	// 	this.get("model").set(this.get("fieldName"), value);
-	// }.observes("value", "model", "fieldName"),
 });
 
 export default RadioFormFieldView;
