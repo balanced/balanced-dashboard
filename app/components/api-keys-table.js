1804abb5017668d6377e119c59eb4a35f10d3111
Kyungmin Kim <kmin905@gmail.com>
2014-12-05 12:51:18 -0800

    Fixes #1552: Show ellipsis when API key is too long for the table

--- app/components/api-keys-table.js
+++ app/components/api-keys-table.js
@@ -9,10 +9,15 @@ var ApiKeysTableComponent = Ember.Component.extend({
 
 	knownApiKeys: Ember.computed.filterBy("keys", "secret"),
 	canDeleteApiKeys: Ember.computed.gt("knownApiKeys.length", 1),
+	showSecret: false,
 
 	actions: {
 		delete: function(key) {
 			this.get("container").lookup("controller:application").send("openModal", "modals/api-key-delete-modal", key);
+		},
+
+		showKeySecret: function() {
+			this.set('showSecret', true);
 		}
 	}
 });
