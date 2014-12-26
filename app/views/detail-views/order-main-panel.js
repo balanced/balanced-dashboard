3e786c383e388512330d5a7315e4d0cf3ecf9962
Kyungmin Kim <kmin905@gmail.com>
2014-12-26 14:46:37 -0800

    Fixes #1583: Combine charges and payouts tab

--- app/views/detail-views/order-main-panel.js
+++ app/views/detail-views/order-main-panel.js
@@ -2,9 +2,7 @@ import Ember from "ember";
 import MainPanelView from "./main-panel";
 
 var OrderMainPanelView = MainPanelView.extend({
-	isChargesTabSelected: Ember.computed.equal("model.selectedTab", "charges"),
-	isPayoutsTabSelected: Ember.computed.equal("model.selectedTab", "payouts"),
-	isLogsEventsTabSelected: Ember.computed.equal("model.selectedTab", "logsEvents"),
+	isActivityTabSelected: true
 });
 
 export default OrderMainPanelView;
