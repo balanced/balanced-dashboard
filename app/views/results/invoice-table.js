732db30780cb4eff1e62b8d5928dec499c32cd94
Kyungmin Kim <kmin905@gmail.com>
2014-12-29 11:15:43 -0800

    Add class names to invoice row

--- app/views/results/invoice-table.js
+++ app/views/results/invoice-table.js
@@ -38,6 +38,7 @@ var InvoiceTableView = ResultsTableView.extend({
 	feesByType: function() {
 		if (this.get("model.isDispute")) {
 			return [{
+				className: "dispute-details-row",
 				type: "Dispute",
 				quantity: "disputes_count",
 				txnAmount: "disputes_total_amount",
@@ -47,12 +48,14 @@ var InvoiceTableView = ResultsTableView.extend({
 		}
 
 		return [{
+				className: "hold-details-row",
 				type: "Hold",
 				quantity: "holds_count",
 				txnAmount: "holds_total_amount",
 				fee: "%@ per hold".fmt(Utils.formatCurrency(this.get("model.hold_fee"))),
 				totalFee: "holds_total_fee"
 			}, {
+				className: "card-debit-details-row",
 				primary: "Debit",
 				secondary: "Cards",
 				quantity: "card_debits_count",
@@ -60,6 +63,7 @@ var InvoiceTableView = ResultsTableView.extend({
 				fee: "%@% of txn + %@".fmt(this.get("model.variable_fee_percentage"), Utils.formatCurrency(this.get("model.card_debit_fixed_fee"))),
 				totalFee: "card_debits_total_fee"
 			}, {
+				className: "bank-account-debit-details-row",
 				primary: "Debit",
 				secondary: "Bank accounts",
 				quantity: "bank_account_debits_count",
@@ -67,13 +71,15 @@ var InvoiceTableView = ResultsTableView.extend({
 				fee: "%@% of txn + $0.30 (%@ cap)".fmt(this.get("model.bank_account_debit_variable_fee_percentage"), Utils.formatCurrency(this.get("model.bank_account_debit_variable_fee_cap"))),
 				totalFee: "bank_account_debits_total_fee"
 			}, {
+				className: "succeeded-bank-account-credit-details-row",
 				primary: "Credit",
 				secondary: "Succeeded",
 				quantity: "bank_account_credits_total_amount",
-				txnAmount: "disputes_total_amount",
+				txnAmount: "bank_account_credits_total_amount",
 				fee: "%@ per credit".fmt(Utils.formatCurrency(this.get("model.bank_account_credit_fee"))),
 				totalFee: "bank_account_credits_total_fee"
 			}, {
+				className: "succeeded-card-credit-details-row",
 				primary: "Credit",
 				secondary: "Push to card",
 				quantity: "card_credits_count",
@@ -81,6 +87,7 @@ var InvoiceTableView = ResultsTableView.extend({
 				fee: "%@ per credit".fmt(Utils.formatCurrency(this.get("model.card_credit_fixed_fee"))),
 				totalFee: "card_credits_total_fee"
 			}, {
+				className: "refund-details-row",
 				type: "Refund",
 				quantity: "refunds_count",
 				txnAmount: "refunds_total_amount",
