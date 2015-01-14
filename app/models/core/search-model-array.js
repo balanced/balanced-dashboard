2f38fb1d92c81f50a44a3a7864bf300d687b157b
cohitre <carlosrr@gmail.com>
2015-01-14 11:54:34 -0800

    Fixing total transactions count to include reversals

--- app/models/core/search-model-array.js
+++ app/models/core/search-model-array.js
@@ -17,9 +17,9 @@ var SearchModelArray = ModelArray.extend(Ember.SortableMixin, {
 	total_orders: readOnly('order'),
 	total_disputes: readOnly('dispute'),
 	total_reversals: readOnly("reversal"),
-	total_transactions: Computed.sumAll('total_credits', 'total_debits', 'total_card_holds', 'total_refunds'),
+	total_transactions: Computed.sumAll('total_credits', 'total_debits', 'total_card_holds', 'total_refunds', "total_reversals"),
 	total_funding_instruments: Computed.sumAll('total_bank_accounts', 'total_cards'),
-	total_results: Computed.sumAll('total_orders', 'total_transactions', 'total_funding_instruments', 'total_customers', "total_reversals")
+	total_results: Computed.sumAll('total_orders', 'total_transactions', 'total_funding_instruments', 'total_customers')
 });
 
 export default SearchModelArray;
