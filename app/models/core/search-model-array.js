0e471fee1048fa80cc0946a16595aabf0f72aad3
cohitre <carlosrr@gmail.com>
2015-01-14 10:46:27 -0800

    Fixing how we calculate total_results to include reversals

--- app/models/core/search-model-array.js
+++ app/models/core/search-model-array.js
@@ -16,9 +16,10 @@ var SearchModelArray = ModelArray.extend(Ember.SortableMixin, {
 	total_cards: readOnly('card'),
 	total_orders: readOnly('order'),
 	total_disputes: readOnly('dispute'),
+	total_reversals: readOnly("reversal"),
 	total_transactions: Computed.sumAll('total_credits', 'total_debits', 'total_card_holds', 'total_refunds'),
 	total_funding_instruments: Computed.sumAll('total_bank_accounts', 'total_cards'),
-	total_results: Computed.sumAll('total_orders', 'total_transactions', 'total_funding_instruments', 'total_customers')
+	total_results: Computed.sumAll('total_orders', 'total_transactions', 'total_funding_instruments', 'total_customers', "total_reversals")
 });
 
 export default SearchModelArray;
