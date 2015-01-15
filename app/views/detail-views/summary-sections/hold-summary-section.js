4bef749f474cd0ebef9c127beeb7b51f69e41ce7
cohitre <carlosrr@gmail.com>
2015-01-15 09:53:25 -0800

    Showing status description on holds page

--- app/views/detail-views/summary-sections/hold-summary-section.js
+++ app/views/detail-views/summary-sections/hold-summary-section.js
@@ -1,6 +1,7 @@
 import SummarySectionView from "./summary-section";
 
 var HoldSummarySectionView = SummarySectionView.extend({
+	statusText: Ember.computed.reads('model.status_description'),
 	linkedResources: function() {
 		return this.resourceLinks("model.description", "model.customer", "model.source");
 	}.property("model.customer", "model.source", "model.description")
