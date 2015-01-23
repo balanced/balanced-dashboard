a8760dd9361195f71af6cde725e598db34c202ee
cohitre <carlosrr@gmail.com>
2015-01-22 17:56:02 -0800

    Fixed charge card modal tests.

--- tests/integration/charge-card-test.js
+++ tests/integration/charge-card-test.js
@@ -34,3 +34,45 @@ test('form validation', 2, function() {
 		.click('button:contains(Debit)')
 		.check("#charge-card .form-group.has-error", 6);
 });
+
+test('can charge a card', function() {
+	var spy = sinon.spy(Adapter, 'create');
+	var tokenizingStub = sinon.stub(balanced.card, 'create');
+	tokenizingStub.callsArgWith(1, {
+		status: 201,
+		cards: [{
+			href: '/cards/' + Testing.CARD_ID
+		}]
+	});
+	visit(Testing.TRANSACTIONS_ROUTE)
+		.click('.page-navigation a:contains(Debit a card)')
+		.fillForm('#charge-card', {
+			name: 'Tarun Chaudhry',
+			number: '4111111111111111',
+			cvv: '123',
+			expiration_date: '12 / 2020',
+			postal_code: '95014',
+			appears_on_statement_as: 'My Charge',
+			description: 'Internal',
+			dollar_amount: '12.00'
+		})
+		.then(function() {
+			var model = App.__container__.lookup("controller:modals-container").get("currentModal.model");
+			model.saveCard = function() {
+				return App.__container__.lookupFactory("model:card").find("/cards/" + Testing.CARD_ID);
+			};
+		})
+		.click('button:contains(Debit)')
+		.then(function() {
+			var args = spy.firstCall.args;
+			ok(spy.calledOnce);
+			deepEqual(args.slice(0, 2), [Models.Debit, "/cards/%@/debits".fmt(Testing.CARD_ID)]);
+			matchesProperties(args[2], {
+				amount: "1200",
+				appears_on_statement_as: 'My Charge',
+				description: 'Internal',
+				source_uri: '/cards/' + Testing.CARD_ID
+			});
+		});
+});
+
