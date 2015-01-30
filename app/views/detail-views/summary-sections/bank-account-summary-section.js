aceb3d89d303a254d055217f65eafe43cfb1d9ac
cohitre <carlosrr@gmail.com>
2015-01-30 11:17:39 -0800

    Adding bank account summary section tests

--- app/views/detail-views/summary-sections/bank-account-summary-section.js
+++ app/views/detail-views/summary-sections/bank-account-summary-section.js
@@ -2,39 +2,53 @@ import SummarySectionView from "./summary-section";
 import Utils from "balanced-dashboard/lib/utils";
 
 var BankAccountSummarySectionView = SummarySectionView.extend({
+	verificationRestartDate: Ember.computed("model.verification.updated_at", function() {
+		var updatedAtDate = this.get("model.verification.updated_at");
+		return moment(new Date(updatedAtDate)).add(3, "days").toDate();
+	}),
+
 	statusText: function() {
 		var status = this.get('model.status');
 
 		if (status === 'pending') {
 			return 'Two deposits have been made to your bank account. Confirm verification by entering the amounts.';
-		} else if (status === 'unverified') {
+		}
+		else if (status === 'unverified') {
 			return 'You may only credit to this bank account. You must verify this bank account to debit from it.';
-		} else if (status === 'unverifiable') {
+		}
+		else if (status === 'unverifiable') {
 			return 'You may only credit to this bank account. This bank account is unverifiable because it\'s not associated with a customer.';
-		} else if (status === 'failed') {
-			return 'We could not verify your bank account. You may restart the verification process after three business days after %@.'.fmt(Utils.humanReadableDateShort(this.get('model.verification.updated_at')));
+		}
+		else if (status === 'failed') {
+			return 'We could not verify your bank account. You may restart the verification process after %@.'.fmt(Utils.humanReadableDate(this.get('verificationRestartDate')));
 		}
 
 		return undefined;
-	}.property('model.status', 'model.verification.updated_at'),
+	}.property('model.status', 'verificationRestartDate'),
 
 	statusButtonModalView: function() {
-		var status = this.get('model.status');
-		if (status === 'unverified') {
-			return this.get("container").lookupFactory("view:modals/verify-bank-account-modal");
-		} else if (status === 'pending') {
-			return this.get("container").lookupFactory("view:modals/bank-account-verification-confirm-modal");
+		var container = this.get("container");
+		if (this.get("isCanStartVerification")) {
+			return container.lookupFactory("view:modals/verify-bank-account-modal");
+		} else if (this.get("isCanConfirmVerification")) {
+			return container.lookupFactory("view:modals/bank-account-verification-confirm-modal");
 		}
 		return undefined;
-	}.property('model.status'),
+	}.property('isCanStartVerification', "isCanConfirmVerification"),
+
+	isCanStartVerification: Ember.computed("model.status", "verificationRestartDate", function() {
+		var status = this.get("model.status");
+		return status === "unverified" || (status === "failed" && this.get("verificationRestartDate") < new Date());
+	}),
+
+	isCanConfirmVerification: Ember.computed.equal("model.status", "pending"),
 
 	statusButtonText: function() {
-		var status = this.get('model.status');
-		if (status === 'unverified' || status === 'pending') {
+		if (this.get("isCanStartVerification") || this.get("isCanConfirmVerification")) {
 			return "Verify";
 		}
 		return undefined;
-	}.property("model.status"),
+	}.property("isCanStartVerification", "isCanConfirmVerification"),
 
 	linkedResources: function() {
 		return this.resourceLinks("model.customer");
---
+++ tests/unit/views/detail-views/summary-sections/bank-account-summary-section-test.coffee
@@ -0,0 +1,31 @@
+`import { test, moduleFor } from "ember-qunit";`
+
+moduleFor("view:detail-views/summary-sections/bank-account-summary-section", "View - BankAccountSummarySectionView")
+
+test "#verificationRestartDate", ->
+	s = this.subject()
+	s.set("model", {
+		verification: {
+			updated_at: "2015-01-30T18:48:13.072962Z"
+		}
+	})
+	deepEqual(s.get("verificationRestartDate"), new Date("2015-02-02T18:48:13.072Z"))
+
+
+test "#statusText", ->
+	s = this.subject()
+
+	t = (statusValue, expectedText) ->
+		s.set("model.status", statusValue)
+		deepEqual(s.get("statusText"), expectedText)
+
+	s.set "model",
+		status: "--------"
+		verification:
+			updated_at: "2015-01-30T18:48:13.072962Z"
+
+	t("xxxxx", undefined)
+	t("pending", 'Two deposits have been made to your bank account. Confirm verification by entering the amounts.')
+	t("unverified", "You may only credit to this bank account. You must verify this bank account to debit from it.")
+	t("unverifiable", "You may only credit to this bank account. This bank account is unverifiable because it's not associated with a customer.")
+	t("failed", "We could not verify your bank account. You may restart the verification process after Feb 2, 2015.")
