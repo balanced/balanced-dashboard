ef9176c0d84ebecf6540935bc4b4a8da27e27dbf
cohitre <carlosrr@gmail.com>
2015-02-20 11:20:14 -0800

    Updating error handling on refund dialog

--- app/views/modals/refund-debit-modal.js
+++ app/views/modals/refund-debit-modal.js
@@ -3,8 +3,9 @@ import ModalBaseView from "./modal-base";
 import Form from "balanced-dashboard/views/modals/mixins/form-modal-mixin";
 import Full from "balanced-dashboard/views/modals/mixins/full-modal-mixin";
 import RefundDebitTransactionFactory from "balanced-dashboard/models/factories/refund-debit-transaction-factory";
+import ObjectActionMixin from "./mixins/object-action-mixin";
 
-var RefundDebitModalView = ModalBaseView.extend(Full, Form, {
+var RefundDebitModalView = ModalBaseView.extend(Full, Form, ObjectActionMixin, {
 	templateName: 'modals/refund-debit-modal',
 	elementId: "refund-debit",
 	title: function() {
@@ -27,21 +28,23 @@ var RefundDebitModalView = ModalBaseView.extend(Full, Form, {
 		});
 	}.property("debit"),
 
-	isSaving: false,
+	save: function(model) {
+		this.executeAction(function() {
+			return model.save();
+		});
+	},
+
+	onModelSaved: function(model) {
+		var applicationController = this.container.lookup("controller:application");
+		applicationController.transitionTo(model.get("route_name"), model);
+		this.getModalNotificationController().clearAlerts();
+		this.close();
+	},
 
 	actions: {
 		save: function() {
 			var self = this;
-			this.set("isSaving", true);
-			this.get("model")
-				.save()
-				.then(function(model) {
-					self.set("isSaving", false);
-					self.get("controller").transitionToRoute(model.get("route_name"), model);
-					self.close();
-				}, function(errors) {
-					self.set("isSaving", false);
-				});
+			this.save(this.get("model"));
 		},
 	}
 });
